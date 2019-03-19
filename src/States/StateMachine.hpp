898a527c97cd077dfdc84e4889e4f00d84ad292a
Czapa10 <Gesio888lol@wp.pl>
2019-03-19 17:46:29 +0100

    StateMachine mockup
    It doesn't compile yet, because there is not State class.

--- src/States/StateMachine.hpp
+++ src/States/StateMachine.hpp
@@ -1,12 +1,43 @@
 #ifndef POPHEAD_STATES_STATEMACHINE_H_
 #define POPHEAD_STATES_STATEMACHINE_H_
 
+#include <memory>
+#include <vector>
+#include <SFML/System.hpp>
+
 namespace PopHead {
 namespace States {
 
-class StateMachie
-{
+using StatePtr = std::unique_ptr<State>;
 
+class StateMachine
+{
+public:
+    StateMachine(StatePtr initialState);
+
+    void changingStatesProcess();
+    void input();
+    void update(sf::Time delta);
+    void pushState(StatePtr state);
+    void popState();
+    void replaceState(StatePtr replacer);
+    void clearStates();
+
+    unsigned int getStatesAmount() const;
+    bool getHideInStateNr(unsigned int nrOfState) const;
+    bool getPauseInStateNr(unsigned int nrOfState) const;
+
+    void setHideInStateNr(unsigned int nrOfState, bool hide);
+    void setPauseInStateNr(unsigned int nrOfState, bool pause);
+
+private:
+    std::vector<StatePtr> mActiveStates;
+    std::vector<StatePtr> mPendingStates;
+
+    bool mIsAdding;
+    bool mIsReplacing;
+    bool mIsRemoving;
+    bool mIsClearing;
 };
 
 }}
