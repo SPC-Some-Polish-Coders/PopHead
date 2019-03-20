a598c893f1e617eb7b850a7efbe53c12885de167
Czapa10 <Gesio888lol@wp.pl>
2019-03-20 15:47:12 +0100

    StateMachine constructor was created

---
+++ src/States/StateMachine.cpp
@@ -0,0 +1,13 @@
+#include "States/StateMachine.hpp"
+
+using PopHead::States::StateMachine;
+
+
+StateMachine::StateMachine()
+:mIsAdding(false)
+,mIsReplacing(false)
+,mIsRemoving(false)
+,mIsClearing(false)
+{
+}
+
