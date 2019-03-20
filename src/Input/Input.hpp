84dadb4eeb7743039b90fd1eda949422c7e977b5
Czapa10 <Gesio888lol@wp.pl>
2019-03-21 00:30:39 +0100

    Input module mock up

--- src/Input/Input.hpp
+++ src/Input/Input.hpp
@@ -1,13 +1,22 @@
 #ifndef POPHEAD_INPUT_INPUT_H_
 #define POPHEAD_INPUT_INPUT_H_
 
+#include "ActionManager.hpp"
+#include "KeyboardManager.hpp"
+#include "MouseManager.hpp"
+
 namespace PopHead {
 namespace Input {
 
+
 class Input
 {
-
+private:
+    ActionManager mAction;
+    KeyboardManager mKeyboard;
+    MouseManager mMouse;
 };
+
 
 }}
 
