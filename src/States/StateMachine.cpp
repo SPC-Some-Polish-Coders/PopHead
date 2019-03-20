89600255e46a807ca12a340fc7b144d68b7a147b
Czapa10 <Gesio888lol@wp.pl>
2019-03-20 15:50:32 +0100

    adding empty methods to StateMachine.cpp

--- src/States/StateMachine.cpp
+++ src/States/StateMachine.cpp
@@ -11,3 +11,63 @@ StateMachine::StateMachine()
 {
 }
 
+void StateMachine::changingStatesProcess()
+{
+
+}
+
+void StateMachine::input()
+{
+
+}
+
+void StateMachine::update(sf::Time delta)
+{
+
+}
+
+void StateMachine::pushState(StatePtr state)
+{
+
+}
+
+void StateMachine::popState()
+{
+
+}
+
+void StateMachine::replaceState(StatePtr replacer)
+{
+
+}
+
+void StateMachine::clearStates()
+{
+
+}
+
+unsigned int StateMachine::getStatesAmount() const
+{
+
+}
+
+bool StateMachine::getHideInStateNr(unsigned int nrOfState) const
+{
+
+}
+
+bool StateMachine::getPauseInStateNr(unsigned int nrOfState) const
+{
+
+}
+
+void StateMachine::setHideInStateNr(unsigned int nrOfState, bool hide)
+{
+
+}
+
+void StateMachine::setPauseInStateNr(unsigned int nrOfState, bool pause)
+{
+
+}
+
