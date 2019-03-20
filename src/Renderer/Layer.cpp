bbce2708c1b3096c651353130294c010a98cffa4
Kamil Lewan <carmel4a97@gmail.com>
2019-03-20 19:44:16 +0100

    Add missed file.

---
+++ src/Renderer/Layer.cpp
@@ -0,0 +1,30 @@
+#include "Renderer/Layer.hpp"
+
+#include "Renderer/LayerID.hpp"
+
+#include "World/Entity/Object.hpp"
+
+using PopHead::Renderer::Layer;
+using PopHead::Renderer::LayerID;
+
+void Layer::addObject( World::Entity::Object* const object )
+{
+    mObjects.emplace_back( object );
+}
+
+void Layer::removeObject( const World::Entity::Object* const object )
+{
+    for(auto it = mObjects.begin(); it != mObjects.end(); ++it )
+        if(*it == object)
+        {
+            mObjects.erase(it);
+            return;
+        }
+}
+
+void Layer::removeObject( std::string name )
+{
+    for(auto it = mObjects.begin(); it != mObjects.end(); ++it )
+        if((*it)->getName() == name)
+            mObjects.erase(it);
+}
