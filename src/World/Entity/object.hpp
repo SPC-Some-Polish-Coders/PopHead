6492d5da861b65f9834fee750d66a824254d9dc2
Czapa10 <Gesio888lol@wp.pl>
2019-03-28 17:17:53 +0100

    Object mock up was created, bad logic in Renderer::draw() was fixed

--- src/World/Entity/object.hpp
+++ src/World/Entity/object.hpp
@@ -1,6 +1,7 @@
 #ifndef POPHEAD_WORLD_OBJECT_H_
 #define POPHEAD_WORLD_OBJECT_H_
 
+#include <SFML/Graphics.hpp>
 #include "entity.hpp"
 #include "Renderer/layerID.hpp"
 
@@ -9,18 +10,36 @@ namespace World {
 namespace Entity {
 
 
-class Object : public Entity
+class Object : public Entity, public sf::Drawable
 {
-  public:
+public:
     Object( Renderer::LayerID layerID );
-    virtual void draw() =0;
-    // virtual void draw( sf::RenderTarget&, sf::RenderStates ) =0;
+
+    virtual void draw( sf::RenderTarget&, sf::RenderStates ) const override = 0;
+
+    void moveTo(sf::Vector2f);
+    virtual void onColision(Object&) = 0;
+
+    void show();
+    void hide();
+
+    virtual void setPosition(sf::Vector2f);
+    virtual void setScale(sf::Vector2f);
+    virtual void setRotation(float angle);
+
+    sf::Vector2f getPosition() const;
+    sf::Vector2f getScale() const;
+    float getRotation() const;
+    ///auto getColision() const -> const ColisionObject&;
 
     inline auto getLayerID() const -> Renderer::LayerID;
 
-  private:
-
-
+private:
+    sf::Vector2f mPosition;
+    sf::Vector2f mScale;
+    float mRotation;
+    ///ColisionObject mColision;
+    bool mVisibility;
     const Renderer::LayerID mLayerID;
 };
 
