52360b9502ea253ce155b2fc13fb3be99a1cc68e
Czapa10 <Gesio888lol@wp.pl>
2019-04-04 00:21:33 +0200

    CharacterMotion struct was created

---
+++ src/World/Entity/Objects/characterMotion.cpp
@@ -0,0 +1,16 @@
+#include "characterMotion.hpp"
+
+using PopHead::World::Entity::CharacterMotion;
+
+CharacterMotion::CharacterMotion()
+{
+    clear();
+}
+
+void CharacterMotion::clear()
+{
+    isMovingLeft = false;
+    isMovingRight = false;
+    isMovingUp = false;
+    isMovingDown = false;
+}
---
+++ src/World/Entity/Objects/characterMotion.hpp
@@ -0,0 +1,23 @@
+#ifndef POPHEAD_WORLD_ENTITY_OBJECTS_CHARACTERMOTION_H_
+#define POPHEAD_WORLD_ENTITY_OBJECTS_CHARACTERMOTION_H_
+
+namespace PopHead{
+namespace World{
+namespace Entity{
+
+
+struct CharacterMotion
+{
+    CharacterMotion();
+    void clear();
+
+    bool isMovingLeft;
+    bool isMovingRight;
+    bool isMovingUp;
+    bool isMovingDown;
+};
+
+
+}}}
+
+#endif // !POPHEAD_WORLD_ENTITY_OBJECTS_CHARACTERMOTION_H_
