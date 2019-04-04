787ce162e3238292adc49eb00a34f8fcc48f0729
Czapa10 <Gesio888lol@wp.pl>
2019-04-04 23:22:25 +0200

    Enemy class was created

---
+++ src/World/Entity/Objects/Characters/enemy.cpp
@@ -0,0 +1,10 @@
+#include "enemy.hpp"
+
+using PopHead::World::Entity::Enemy;
+
+#if 0
+void Enemy::onColision(Object&)
+{
+
+}
+#endif // 0
---
+++ src/World/Entity/Objects/Characters/enemy.hpp
@@ -0,0 +1,26 @@
+#ifndef POPHEAD_WORLD_ENTITY_OBJECTS_CHARACTERS_ENEMY_H_
+#define POPHEAD_WORLD_ENTITY_OBJECTS_CHARACTERS_ENEMY_H_
+
+#include "World/Entity/Objects/character.hpp"
+#include <set>
+
+namespace PopHead{
+namespace World{
+namespace Entity{
+
+
+class Enemy : public Character
+{
+public:
+    ///void onColision(Object&) override;
+
+    ///auto getDamageWeaknesses() const -> const std::set<DamageWeakness>;
+
+protected:
+    ///std::set<DamageWeakness> mDamageWeaknesses;
+};
+
+
+}}}
+
+#endif // !POPHEAD_WORLD_ENTITY_OBJECTS_CHARACTERS_ENEMY_H_
