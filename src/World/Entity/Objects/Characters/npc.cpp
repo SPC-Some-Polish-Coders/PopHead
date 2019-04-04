bd21900ce8618604d41958ec8283d1a2c8b92c20
Czapa10 <Gesio888lol@wp.pl>
2019-04-04 22:30:50 +0200

    NPC class was created

---
+++ src/World/Entity/Objects/Characters/npc.cpp
@@ -0,0 +1,28 @@
+#include "npc.hpp"
+
+using PopHead::World::Entity::NPC;
+
+
+NPC::NPC(Base::GameData* gameData, std::string name)
+:Character(gameData, name)
+{
+}
+
+void NPC::input()
+{
+    #if 0
+    if(mGameData->getInput().getAction().isActionJustPressed("talk with NPC"))
+        wasDialogueButtonClicked = true;
+    #endif // 0
+}
+
+void NPC::update(sf::Time delta)
+{
+    if(wasDialogueButtonClicked)
+        talk();
+}
+
+void NPC::talk()
+{
+
+}
