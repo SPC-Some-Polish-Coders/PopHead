86e8fc6f072bb6c08bef3b5d5a829cd67db9f6a6
Prezes <piotrbrzostowski96@gmail.com>
2019-04-08 11:22:58 +0200

    Fixed setters and getters in object.cpp

--- src/World/Entity/object.cpp
+++ src/World/Entity/object.cpp
@@ -15,54 +15,24 @@ void Object::moveTo(sf::Vector2f)
 {
 }
 
-void Object::setVisibility(bool visibility)
-{
-    mVisibility = visibility;
-}
+void Object::setVisibility(bool visibility) { mVisibility = visibility; }
 
-void Object::setPosition(sf::Vector2f pos)
-{
-    mPosition = pos;
-}
+void Object::setPosition(sf::Vector2f pos) { mPosition = pos; }
 
-void Object::setScale(sf::Vector2f scale)
-{
-    mScale = scale;
-}
+void Object::setScale(sf::Vector2f scale) { mScale = scale; }
 
-void Object::setRotation(float angle)
-{
-    mRotation = angle;
-}
+void Object::setRotation(float angle) { mRotation = angle; }
 
-sf::Vector2f Object::getPosition() const
-{
-    return mPosition;
-}
+sf::Vector2f Object::getPosition() const { return mPosition; }
 
-sf::Vector2f Object::getScale() const
-{
-    return mScale;
-}
+sf::Vector2f Object::getScale() const { return mScale; }
 
-float Object::getRotation() const
-{
-    return mRotation;
-}
+float Object::getRotation() const { return mRotation; }
 
 #if 0
-auto Object::getColision() const -> const ColisionObject&
-{
-    return mColision;
-}
+auto Object::getColision() const -> const ColisionObject& { return mColision; }
 #endif // 0
 
-auto Object::getLayerID() const -> PopHead::Renderer::LayerID
-{
-    return mLayerID;
-}
+auto Object::getLayerID() const -> PopHead::Renderer::LayerID { return mLayerID; }
 
-bool Object::getVisibility() const
-{
-    return mVisibility;
-}
+bool Object::getVisibility() const { return mVisibility; }
