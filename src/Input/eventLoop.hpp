ba9357df92b09a391beabf87e9bce6187dfd68d7
Czapa10 <Gesio888lol@wp.pl>
2019-04-11 17:12:35 +0200

    making static variables in EventLoop

--- src/Input/eventLoop.hpp
+++ src/Input/eventLoop.hpp
@@ -15,9 +15,20 @@ public:
 
 private:
     static Base::GameData* mGameData;
+
+    static bool isKeyPressed;
+    static bool isKeyReleased;
+    static bool isMouseButtonPressed;
+    static bool isMouseButtonReleased;
+    static bool hasMouseMoved;
 };
 
 Base::GameData* EventLoop::mGameData;
+bool EventLoop::isKeyPressed;
+bool EventLoop::isKeyReleased;
+bool EventLoop::isMouseButtonPressed;
+bool EventLoop::isMouseButtonReleased;
+bool EventLoop::hasMouseMoved;
 
 
 }}
