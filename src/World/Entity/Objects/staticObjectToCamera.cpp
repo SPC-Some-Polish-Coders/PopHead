7fa11dbd5f303743a6f3d8b8b221cd763352972a
Czapa <gesio888lol@wp.pl>
2019-04-29 19:05:58 +0200

    temporary class StaticObjectToCamera was created.

---
+++ src/World/Entity/Objects/staticObjectToCamera.cpp
@@ -0,0 +1,17 @@
+#include "staticObjectToCamera.hpp"
+
+#include <SFML/Graphics.hpp>
+
+using PopHead::World::Entity::StaticObjectToCamera;
+
+StaticObjectToCamera::StaticObjectToCamera(PopHead::Base::GameData* gameData)
+:Object(gameData, "staticObjectToCamera", Renderer::LayerID::GUI)
+,mShape(sf::Vector2f(300, 100))
+{
+	mShape.setFillColor(sf::Color::Red);
+}
+
+void StaticObjectToCamera::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(mShape, states);
+}
---
+++ src/World/Entity/Objects/staticObjectToCamera.hpp
@@ -0,0 +1,28 @@
+#ifndef POPHEAD_WORLD_ENTITY_OBJECTS_STATICOBJECTTOCAMERA_H_
+#define POPHEAD_WORLD_ENTITY_OBJECTS_STATICOBJECTTOCAMERA_H_
+
+#include "World/Entity/object.hpp"
+
+///This class is only for test purposes
+///It should be deleted when GUI is made
+
+namespace PopHead {
+namespace World {
+namespace Entity {
+
+
+class StaticObjectToCamera : public Object
+{
+public:
+	StaticObjectToCamera(PopHead::Base::GameData*);
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+	sf::RectangleShape mShape;
+};
+
+
+}}}
+
+#endif //!POPHEAD_WORLD_ENTITY_OBJECTS_STATICOBJECTTOCAMERA_H_
