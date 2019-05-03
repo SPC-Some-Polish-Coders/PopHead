bc6cd9529e254d2c292f3d86d3a91db0db456065
Czapa <gesio888lol@wp.pl>
2019-05-03 15:10:06 +0200

    small style refactor in SpriteSheetData

--- src/Resources/spriteSheetData.hpp
+++ src/Resources/spriteSheetData.hpp
@@ -5,12 +5,14 @@ namespace PopHead {
 namespace Resources {
 
 
-namespace SpriteSheetData {
+namespace SpriteSheetData
+{
 	static constexpr unsigned PLAYER_WIDTH = 48;
 	static constexpr unsigned PLAYER_HEIGHT = 48;
 	static constexpr unsigned PLAYER_FRAMES_COUNT = 3;
 }
 
+
 }}
 
-#endif
+#endif // !POPHEAD_RESOURCE_SPRITESHEETDATA_H_
