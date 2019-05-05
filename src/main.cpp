95a0d36c8637faae7a216ab345d130e1e1c8eda6
huberti1 <01huberti@gmail.com>
2019-05-05 08:24:11 +0200

    Added error message box for unhandled exceptions.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,29 @@
 
 #include "Logs/Logs.hpp"
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif // _WIN32
+#include <stdexcept>
+#include <cstdlib>
+
 int main()
 {
-	PopHead::LOG(LogType::INFO, ModuleID::None, "start executing PopHead!");
-
-	PopHead::Base::Game();
-    return 0;
+	try {
+		PopHead::LOG(LogType::INFO, ModuleID::None, "start executing PopHead!");
+		PopHead::Base::Game();
+	}
+	catch (const std::exception& e) {
+#ifdef _WIN32
+		MessageBoxA(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
+#endif // _WIN32
+		return EXIT_FAILURE;
+	}
+	catch (...) {
+#ifdef _WIN32
+		MessageBoxA(nullptr, "Unknown error occurred!", "Error", MB_OK | MB_ICONERROR);
+#endif // _WIN32
+		return EXIT_FAILURE;
+	}
+    return EXIT_SUCCESS;
 }
