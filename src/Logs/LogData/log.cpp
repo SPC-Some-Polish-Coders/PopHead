e296b4d12b8a66de8fbfcee45ebdc87a55cbff0d
Szymon Strugala <szymonstrugala23@gmail.com>
2019-05-06 21:47:00 +0200

    Log constructor is now working. Now it needs to be refactored

--- src/Logs/LogData/log.cpp
+++ src/Logs/LogData/log.cpp
@@ -15,19 +15,17 @@ Log::Log(LogType logType, const char* const filePath, const std::string& message
 	// TODO: PH_ASSERT(begin != std::string::npos, "Module location cannot be found");
 	//std::cout << std::strlen("PopHead\\src\\");
 	begin += std::strlen("PopHead\\src\\");
-	//std::cout << "Position is: " << begin;
 	if (moduleName.find("\\", begin) == std::string::npos)
 	{
 		moduleName = "None";
-		std::cout << "NO MODULE\n";
 	}
 	else
 	{
 		std::size_t end = moduleName.find("\\", begin);
+		moduleName.erase(end);
+		moduleName.erase(0, begin);
 		if (end == std::string::npos)
 			std::cout << "ERROR\n";
-		std::cout << begin << '\n';
-		std::cout << end << '\n';
 		std::cout << moduleName << '\n';
 	}
 
