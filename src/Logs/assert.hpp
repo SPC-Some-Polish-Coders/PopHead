64cc72bdbd574108e78760935f82fa23fc00da6a
huberti1 <01huberti@gmail.com>
2019-05-07 12:49:39 +0200

    Added more macros.

--- src/Logs/assert.hpp
+++ src/Logs/assert.hpp
@@ -2,6 +2,7 @@
 #define POPHEAD_LOGS_ASSERT_H_
 
 #include "Logs/logger.hpp"
+#include <iostream>
 
 #ifdef _MSC_VER
 #define PH_BREAKPOINT() __debugbreak()
@@ -11,8 +12,12 @@
 
 #ifdef PH_RELEASE
 #define PH_ASSERT(expression, message) ((void)0)
+#define PH_ASSERT_WITH_COUT(expression) ((void)0)
+#define PH_ASSERT_NO_LOG(expression) ((void)0)
 #else
 #define PH_ASSERT(expression, message) (void)((expression) || (PH_LOG(PopHead::Logs::LogType::Error, message), PH_BREAKPOINT(), 0))
+#define PH_ASSERT_WITH_COUT(expression, message) (void)((expression) || (std::cout<<"(PH_ASSERT_WITH_COUT)ERROR: "<<message<<std::endl, PH_BREAKPOINT(), 0))
+#define PH_ASSERT_NO_LOG(expression) (void)((expression) || (PH_BREAKPOINT(), 0))
 #endif // PH_RELEASE
 
 #endif // !POPHEAD_LOGS_ASSERT_H_
