8332968360cb5a3edf4788ab08e68170424d4cc7
huberti1 <01huberti@gmail.com>
2019-05-07 12:50:21 +0200

    Improved Log().

--- src/Logs/LogData/log.cpp
+++ src/Logs/LogData/log.cpp
@@ -1,4 +1,5 @@
 #include "log.hpp"
+#include "Logs/assert.hpp"
 #include <cstring>
 #include <iostream>
 
@@ -8,24 +9,19 @@ using PopHead::Logs::Log;
 Log::Log(LogType logType, const char* const filePath, const std::string& message)
 	: message(message), moduleName(std::string(filePath)), type(logType)
 {
-	//G:\PopHead\src\main.cpp
-	//G:\PopHead\src\States\stateMachine.cpp
-	std::size_t begin = moduleName.rfind("PopHead\\src");
-	//NOTE: We can't use PH_ASSERT here, becouse it uses Log, so it can cause recursion
-	//PH_ASSERT(begin != std::string::npos, "Module location cannot be found");
-	begin += std::strlen("PopHead\\src\\");
-	if (moduleName.find("\\", begin) == std::string::npos)
-	{
-		moduleName = "None";
-	}
-	else
-	{
-		std::size_t end = moduleName.find("\\", begin);
-		moduleName.erase(end);
-		moduleName.erase(0, begin);
-		if (end == std::string::npos)
-			std::cout << "ERROR\n";
-		std::cout << moduleName << '\n';
-	}
+	// WARNING: Don't use PH_ASSERT here, becouse it uses Log, so it can cause recursion
+	// TODO: Allow this also on Linux (Linux has forward slash instead of backslash)
 
+	const char* const PROJECT_SOURCE_PATH = "PopHead\\src";
+	std::size_t begin = moduleName.find(PROJECT_SOURCE_PATH);
+	PH_ASSERT_WITH_COUT(begin != std::string::npos, "Module location cannot be found");
+	PH_ASSERT_WITH_COUT(moduleName.find(PROJECT_SOURCE_PATH, begin + 1) == std::string::npos, "Move folder with project to another location");
+	begin += std::strlen(PROJECT_SOURCE_PATH) + 1;
+	PH_ASSERT_WITH_COUT(begin < moduleName.size(), "There should be at least one letter after source path");
+
+	std::size_t end = moduleName.find("\\", begin + 1);
+	if (end == std::string::npos)
+		moduleName = "None";
+	else
+		moduleName = moduleName.substr(begin, end - begin);
 }
