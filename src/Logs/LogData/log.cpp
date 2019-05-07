2544c4775cc7b1fba2ce45d1bc9926e2181c2372
huberti1 <01huberti@gmail.com>
2019-05-07 14:10:40 +0200

    Deleted unnecessary macros.

--- src/Logs/LogData/log.cpp
+++ src/Logs/LogData/log.cpp
@@ -12,13 +12,21 @@ Log::Log(LogType logType, const char* const filePath, const std::string& message
 	// WARNING: Don't use PH_ASSERT here, becouse it uses Log, so it can cause recursion
 	// TODO: Allow this also on Linux (Linux has forward slash instead of backslash)
 
-	const char* const PROJECT_SOURCE_PATH = "PopHead\\src";
-	std::size_t begin = moduleName.find(PROJECT_SOURCE_PATH);
-	PH_ASSERT_WITH_COUT(begin != std::string::npos, "Module location cannot be found");
-	PH_ASSERT_WITH_COUT(moduleName.find(PROJECT_SOURCE_PATH, begin + 1) == std::string::npos, "Move folder with project to another location");
-	begin += std::strlen(PROJECT_SOURCE_PATH) + 1;
-	PH_ASSERT_WITH_COUT(begin < moduleName.size(), "There should be at least one letter after source path");
-
+	const char* const SOURCE_PATH = "PopHead\\src";
+	std::size_t begin = moduleName.find(SOURCE_PATH);
+	if (begin == std::string::npos) {
+		std::cout << "[Log::Log()] Module location cannot be found" << std::endl;
+		PH_BREAKPOINT();
+	}
+	if (moduleName.find(SOURCE_PATH, begin + 1) != std::string::npos) {
+		std::cout << "[Log::Log()] Move folder with project to another location" << std::endl;
+		PH_BREAKPOINT();
+	}
+	begin += std::strlen(SOURCE_PATH) + 1;
+	if (begin >= moduleName.size()) {
+		std::cout << "[Log::Log()] There should be at least one letter after source path" << std::endl;
+		PH_BREAKPOINT();
+	}
 	std::size_t end = moduleName.find("\\", begin + 1);
 	if (end == std::string::npos)
 		moduleName = "None";
