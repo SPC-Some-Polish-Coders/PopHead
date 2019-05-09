b856dab47f57bc8b42e563b08ebfe8c6820f24fe
Szymon Strugala <szymonstrugala23@gmail.com>
2019-05-09 20:50:55 +0200

    Slashes and backslashes corrected in 'PH_PATH_SEPARATOR' macro

--- src/Utilities/parser.hpp
+++ src/Utilities/parser.hpp
@@ -2,9 +2,9 @@
 #define POPHEAD_UTILITIES_PARSER_H_
 
 #ifdef _WIN32
-#define PH_PATH_SEPARATOR std::string("/")
-#else
 #define PH_PATH_SEPARATOR std::string("\\")
+#else
+#define PH_PATH_SEPARATOR std::string("/")
 #endif
 
 #include <string>
