067187164b472947dad59cc58f0fef846486a850
Czapa <gesio888lol@wp.pl>
2019-05-12 23:33:06 +0200

    big refactor in KinematicCollisionHandler::handleKinematicCollision()

--- src/Physics/kinematicCollisionHandler.cpp
+++ src/Physics/kinematicCollisionHandler.cpp
@@ -8,54 +8,36 @@ void KinematicCollisionHandler::handleKinematicCollision(CollisionBody* kinemati
 {
     float mass1 = kinematicBodyFirst->getMass();
     float mass2 = kinematicBodySecond->getMass();
+
     if(mass1 == mass2)
         return;
 
-    sf::Vector2f kPos1;
-    sf::Vector2f kPos2;
-    kPos1 = kinematicBodyFirst->getPosition();
-    kPos2 = kinematicBodySecond->getPosition();
-    kPos1.x += 16;
-    kPos1.y += 16;
-    kPos2.x += 16;
-    kPos2.y += 16;
-    sf::Vector2f sides;
-    sides.x =kPos1.x-kPos2.x;
-    sides.y =kPos1.y-kPos2.y;
+    sf::Vector2f kPos1 = kinematicBodyFirst->getPosition() + sf::Vector2f(16, 16);
+    sf::Vector2f kPos2 = kinematicBodySecond->getPosition() + sf::Vector2f(16, 16);
+
+    sf::Vector2f sides = kPos1 - kPos2;
+
     sf::Vector2f vector;
-    float sideC = sqrt(sides.x*sides.x+sides.y*sides.y);
-    float div = sides.x/sideC;
+    float sideC = sqrt(sides.x * sides.x  +  sides.y * sides.y);
+    float div = sides.x / sideC;
     if(div < 0)
-        div*=-1;
-    if(kPos1.x > kPos2.x)
-    {
-        vector.x = 1*div;
-    }
-    else
-    {
-        vector.x = -1*div;
-    }
-    if(kPos1.y > kPos2.y)
-    {
-        vector.y = 1*(1-div);
-    }
-    else
-    {
-        vector.y = -1*(1-div);
-    }
+        div *= -1;
+
+	vector.x = kPos1.x > kPos2.x ? div : -1 * div;
+	vector.y = kPos1.y > kPos2.y ? 1 - div : -1 * (1 - div);
 
     float force;
     sf::Vector2f forceVector;
     if(mass1 > mass2)
     {
-        force = (mass1-mass2)*12.5;
-        forceVector = sf::Vector2f(force*vector.x,force*vector.y);
+        force = (mass1-mass2) * 12.5;
+        forceVector = sf::Vector2f(force * vector.x, force * vector.y);
         kinematicBodySecond->setForceVector(forceVector);
     }
     else if(mass2 > mass1)
     {
-        force = (mass2-mass1)*12.5;
-        forceVector = sf::Vector2f(force*vector.x,force*vector.y);
+        force = (mass2 - mass1) * 12.5;
+        forceVector = sf::Vector2f(force * vector.x, force * vector.y);
         kinematicBodyFirst->setForceVector(forceVector);
     }
 }
