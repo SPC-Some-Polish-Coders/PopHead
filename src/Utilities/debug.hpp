62899158ab815c07cf156c4bf96e962f2bda2689
huberti1 <01huberti@gmail.com>
2019-05-26 22:09:36 +0200

    Cleanup.

--- src/Utilities/debug.hpp
+++ src/Utilities/debug.hpp
@@ -3,16 +3,16 @@
 
 #include "Logs/logger.hpp"
 
-#define PH_LOG(logType, message)\
-	PopHead::Logs::Logger::getLogger().writeLog(PopHead::Logs::LogData{message, PopHead::Utilities::Parser::toModuleName(std::string(__FILE__)), logType})
-
-#define PH_EXCEPTION(message) (void)(PH_LOG(PopHead::Logs::LogType::Error, message), PH_BREAKPOINT(), throw std::runtime_error(message), 0)
-
 #ifdef _MSC_VER
 #define PH_BREAKPOINT() __debugbreak()
 #else
 #define PH_BREAKPOINT() ((void)0)
 #endif // _MSC_VER
+
+#define PH_LOG(logType, message)\
+	PopHead::Logs::Logger::getLogger().writeLog(PopHead::Logs::LogData{message, PopHead::Utilities::Parser::toModuleName(std::string(__FILE__)), logType})
+
+#define PH_EXCEPTION(message) (void)(PH_LOG(PopHead::Logs::LogType::Error, message), PH_BREAKPOINT(), throw std::runtime_error(message), 0)
 
 #ifdef PH_RELEASE
 #define PH_ASSERT(expression, message) ((void)0)
