7302d3dd87fd7028ac64315a7e9041ffcd110029
Czapa <gesio888lol@wp.pl>
2019-05-29 16:46:51 +0200

    MusicPlayer class was created.

--- src/Audio/Music/musicPlayer.hpp
+++ src/Audio/Music/musicPlayer.hpp
@@ -1,13 +1,30 @@
 #ifndef POPHEAD_AUDIO_MUSICPLAYER_H_
 #define POPHEAD_AUDIO_MUSICPLAYER_H_
 
+#include <SFML/Audio.hpp>
+
 namespace PopHead {
 namespace Audio {
 
+
 class MusicPlayer
 {
+public:
+	MusicPlayer();
+	~MusicPlayer();
 
+	void play(std::string filePath);
+	void stop();
+
+	void setPaused(bool pause);
+	void setVolume(float volume);
+	float getVolume() { return mVolume; }
+
+private:
+	sf::Music mMusic;
+	float mVolume;
 };
+
 
 }}
 
