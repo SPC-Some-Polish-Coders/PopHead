4f82a8ece95dde6f07bb91321bbde2a7798d508f
huberti1 <01huberti@gmail.com>
2019-06-02 20:24:01 +0200

    Cleanup in Math.

--- src/Utilities/math.hpp
+++ src/Utilities/math.hpp
@@ -8,14 +8,16 @@ namespace PopHead {
 namespace Utilities {
 
 namespace Math {
-	inline auto getRightBound(sf::FloatRect bounds) -> float { return bounds.left + bounds.width; }
+	inline float getRightBound(sf::FloatRect bounds) 
+	{ return bounds.left + bounds.width; }
 
-	inline auto getBottomBound(sf::FloatRect bounds) -> float { return bounds.top + bounds.height; }
+	inline float getBottomBound(sf::FloatRect bounds)  
+	{ return bounds.top + bounds.height; }
 
-	inline auto getCenter(sf::FloatRect bounds) -> sf::Vector2f
+	inline sf::Vector2f getCenter(sf::FloatRect bounds)
 	{ return sf::Vector2f(bounds.left + bounds.width / 2.f, bounds.top + bounds.height / 2.f); }
 
-	inline auto lerp(sf::Vector2f source, sf::Vector2f destination, float speed) -> sf::Vector2f
+	inline sf::Vector2f lerp(sf::Vector2f source, sf::Vector2f destination, float speed)
 	{
 		PH_ASSERT(speed >= 0.f, "Speed cannot be less than 0");
 		return source + (destination - source) * speed;
