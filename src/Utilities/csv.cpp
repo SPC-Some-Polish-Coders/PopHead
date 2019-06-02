6ffde37a0729f5ca1d99ce19b33075d90a35538a
huberti1 <01huberti@gmail.com>
2019-06-02 23:49:24 +0200

    Added some common types to csv.

--- src/Utilities/csv.cpp
+++ src/Utilities/csv.cpp
@@ -1,4 +1,5 @@
 #include "csv.hpp"
+#include "Utilities/cast.hpp"
 #include <sstream>
 
 /*
@@ -9,13 +10,35 @@
 	- Allow embedded double-quote characters: Foo, bar, "Something, ""realy"" cool"
 */
 
-std::vector<int> PopHead::Utilities::Csv::toIntValues(const std::string& csv)
+std::vector<std::string> PopHead::Utilities::Csv::toStrings(const std::string& csv)
+{
+	std::istringstream iss(csv);
+	std::vector<std::string> values;
+	std::string temp;
+	while (std::getline(iss, temp, ',')) 
+		values.push_back(temp);
+	return values;
+}
+
+std::vector<int> PopHead::Utilities::Csv::toInts(const std::string& csv)
 {
 	std::istringstream iss(csv);
 	std::vector<int> values;
 	std::string temp;
 	while (std::getline(iss, temp, ',')) {
 		const int value = std::stoi(temp);
+		values.push_back(value);
+	}
+	return values;
+}
+
+std::vector<unsigned> PopHead::Utilities::Csv::toUnsigneds(const std::string& csv)
+{
+	std::istringstream iss(csv);
+	std::vector<unsigned> values;
+	std::string temp;
+	while (std::getline(iss, temp, ',')) {
+		const unsigned value = Cast::toUnsigned(temp);
 		values.push_back(value);
 	}
 	return values;
--- src/Utilities/csv.hpp
+++ src/Utilities/csv.hpp
@@ -8,7 +8,11 @@ namespace PopHead {
 namespace Utilities {
 
 namespace Csv {
-	std::vector<int> toIntValues(const std::string& csv);
+	std::vector<std::string> toStrings(const std::string& csv);
+
+	std::vector<int> toInts(const std::string& csv);
+
+	std::vector<unsigned> toUnsigneds(const std::string& csv);
 }
 
 }}
