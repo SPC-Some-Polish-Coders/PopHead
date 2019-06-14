cbee1d8a734658e751e5cc5224b21cebf9e9f759
huberti1 <01huberti@gmail.com>
2019-06-14 14:12:13 +0200

    Big cleanup in Map.

--- src/World/Entity/Objects/map.cpp
+++ src/World/Entity/Objects/map.cpp
@@ -4,10 +4,9 @@
 #include "Utilities/csv.hpp"
 #include "Utilities/math.hpp"
 
-ph::Map::Map(GameData* gameData, std::string name, float scale)
+ph::Map::Map(GameData* gameData, std::string name)
 	:Object(gameData, name, LayerID::floorEntities)
 {
-	// TODO: Scale tiles? (scale funtion paramiter)
 }
 
 void ph::Map::loadFromFile(const std::string& filename)
@@ -15,41 +14,53 @@ void ph::Map::loadFromFile(const std::string& filename)
 	Xml document;
 	document.loadFromFile(filename);
 	const Xml mapNode = document.getChild("map");
-
-	const std::string orientation = mapNode.getAttribute("orientation").toString();
-	if (orientation != "orthogonal")
-		PH_EXCEPTION("Used unsupported map orientation: " + orientation);
-	const std::string infinite = mapNode.getAttribute("infinite").toString();
-	if (infinite != "0")
-		PH_EXCEPTION("Infinite maps are not supported");
-
-	const sf::Vector2u mapSize(
-		mapNode.getAttribute("width").toUnsigned(),
-		mapNode.getAttribute("height").toUnsigned()
-	);
-	const sf::Vector2u tileSize(
-		mapNode.getAttribute("tilewidth").toUnsigned(),
-		mapNode.getAttribute("tileheight").toUnsigned()
-	);
-
-	const std::vector<Xml> tilesetNodes = mapNode.getChildren("tileset");
-	if (tilesetNodes.size() == 0)
-		PH_LOG(LogType::Warning, "Map doesn't have any tilesets: " + filename);
-	TilesetsData tilesets = getTilesetsData(tilesetNodes);
-
-	const std::vector<Xml> layerNodes = mapNode.getChildren("layer");
-	if (layerNodes.size() == 0)
-		PH_LOG(LogType::Warning, "Map doesn't have any layers: " + filename);
+	checkMapSupport(mapNode);
+	const sf::Vector2u mapSize = getMapSize(mapNode);
+	const sf::Vector2u tileSize = getTileSize(mapNode);
+	const std::vector<Xml> tilesetNodes = getTilesetNodes(mapNode);
+	const TilesetsData tilesets = getTilesetsData(tilesetNodes);
+	const std::vector<Xml> layerNodes = getLayerNodes(mapNode);
 	mTiles.reserve(mapSize.x * mapSize.y * layerNodes.size());
 	for (const Xml& layerNode : layerNodes) {
 		const Xml dataNode = layerNode.getChild("data");
-		const std::string encoding = dataNode.getAttribute("encoding").toString();
-		if (encoding != "csv")
-			PH_EXCEPTION("Used unsupported data encoding: " + encoding);
-
-		const std::vector<unsigned> values = Csv::toUnsigneds(dataNode.toString());
-		loadTiles(values, tilesets, mapSize, tileSize);
+		const std::vector<unsigned> globalTileIds = toGlobalTileIds(dataNode);
+		loadTiles(globalTileIds, tilesets, mapSize, tileSize);
 	}
+}
+
+void ph::Map::checkMapSupport(const Xml& mapNode) const
+{
+	const std::string orientation = mapNode.getAttribute("orientation").toString();
+	if (orientation != "orthogonal")
+		PH_EXCEPTION("Used unsupported map orientation: " + orientation);
+
+	const std::string infinite = mapNode.getAttribute("infinite").toString();
+	if (infinite != "0")
+		PH_EXCEPTION("Infinite maps are not supported");
+}
+
+sf::Vector2u ph::Map::getMapSize(const Xml& mapNode) const
+{
+	return sf::Vector2u(
+		mapNode.getAttribute("width").toUnsigned(),
+		mapNode.getAttribute("height").toUnsigned()
+	);
+}
+
+sf::Vector2u ph::Map::getTileSize(const Xml& mapNode) const
+{
+	return sf::Vector2u(
+		mapNode.getAttribute("tilewidth").toUnsigned(),
+		mapNode.getAttribute("tileheight").toUnsigned()
+	);
+}
+
+std::vector<ph::Xml> ph::Map::getTilesetNodes(const Xml& mapNode) const
+{
+	const std::vector<Xml> tilesetNodes = mapNode.getChildren("tileset");
+	if (tilesetNodes.size() == 0)
+		PH_LOG(LogType::Warning, "Map doesn't have any tilesets");
+	return tilesetNodes;
 }
 
 ph::Map::TilesetsData ph::Map::getTilesetsData(const std::vector<Xml>& tilesetNodes) const
@@ -70,38 +81,55 @@ ph::Map::TilesetsData ph::Map::getTilesetsData(const std::vector<Xml>& tilesetNo
 	TilesetsData tilesets;
 	tilesets.sources.reserve(tilesetNodes.size());
 	tilesets.columnsCounts.reserve(tilesetNodes.size());
-	tilesets.globalIds.reserve(tilesetNodes.size());
+	tilesets.firstGlobalTileIds.reserve(tilesetNodes.size());
 	tilesets.tileCounts.reserve(tilesetNodes.size());
-	for (std::size_t i = 0; i < tilesetNodes.size(); ++i) {
-		tilesets.columnsCounts.push_back(tilesetNodes[i].getAttribute("columns").toUnsigned());
-		tilesets.globalIds.push_back(tilesetNodes[i].getAttribute("firstgid").toUnsigned());
-		tilesets.tileCounts.push_back(tilesetNodes[i].getAttribute("tilecount").toUnsigned());
-
-		const Xml imageNode = tilesetNodes[i].getChild("image");
-		tilesets.sources.push_back(imageNode.getAttribute("source").toString());
-		tilesets.sources[i] = Path::toFilename(tilesets.sources[i], '/');
+	for (const Xml& tilesetNode : tilesetNodes) {
+		tilesets.columnsCounts.push_back(tilesetNode.getAttribute("columns").toUnsigned());
+		tilesets.firstGlobalTileIds.push_back(tilesetNode.getAttribute("firstgid").toUnsigned());
+		tilesets.tileCounts.push_back(tilesetNode.getAttribute("tilecount").toUnsigned());
+		const Xml imageNode = tilesetNode.getChild("image");
+		std::string source = imageNode.getAttribute("source").toString();
+		source = Path::toFilename(source, '/');
+		tilesets.sources.push_back(source);
 	}
 	return tilesets;
 }
 
-void ph::Map::loadTiles(const std::vector<unsigned>& values,
-						const TilesetsData& tilesets,
-						sf::Vector2u mapSize,
-						sf::Vector2u tileSize)
+std::vector<ph::Xml> ph::Map::getLayerNodes(const Xml& mapNode) const
 {
-	for (std::size_t i = 0; i < values.size(); ++i) {
-		if (hasTile(values[i])) {
-			const std::size_t j = findTilesetIndex(values[i], tilesets);
+	const std::vector<Xml> layerNodes = mapNode.getChildren("layer");
+	if (layerNodes.size() == 0)
+		PH_LOG(LogType::Warning, "Map doesn't have any layers");
+	return layerNodes;
+}
+
+std::vector<unsigned> ph::Map::toGlobalTileIds(const Xml& dataNode) const
+{
+	const std::string encoding = dataNode.getAttribute("encoding").toString();
+	if (encoding == "csv")
+		return Csv::toUnsigneds(dataNode.toString());
+	PH_EXCEPTION("Used unsupported data encoding: " + encoding);
+}
+
+void ph::Map::loadTiles(
+	const std::vector<unsigned>& globalTileIds,
+	const TilesetsData& tilesets,
+	sf::Vector2u mapSize,
+	sf::Vector2u tileSize)
+{
+	for (std::size_t i = 0; i < globalTileIds.size(); ++i) {
+		if (hasTile(globalTileIds[i])) {
+			const std::size_t j = findTilesetIndex(globalTileIds[i], tilesets);
 			if (j == std::string::npos) {
-				PH_LOG(LogType::Warning, "It was not possible to find tileset for value " + values[i]);
+				PH_LOG(LogType::Warning, "It was not possible to find tileset for " + globalTileIds[i]);
 				continue;
 			}
-
-			sf::Vector2u tilePosition = Math::toTwoDimensional(values[i] - tilesets.globalIds[j], tilesets.columnsCounts[j]);
-			tilePosition.x *= tileSize.x;
-			tilePosition.y *= tileSize.y;
+			const unsigned tileId = globalTileIds[i] - tilesets.firstGlobalTileIds[j];
+			sf::Vector2u tileRectPosition = Math::toTwoDimensional(tileId, tilesets.columnsCounts[j]);
+			tileRectPosition.x *= tileSize.x;
+			tileRectPosition.y *= tileSize.y;
 			const sf::IntRect tileRect(
-				static_cast<sf::Vector2i>(tilePosition),
+				static_cast<sf::Vector2i>(tileRectPosition),
 				static_cast<sf::Vector2i>(tileSize)
 			);
 			sf::Sprite tile(mGameData->getTextures().get(pathToMapTextures + tilesets.sources[j]), tileRect);
@@ -109,17 +137,17 @@ void ph::Map::loadTiles(const std::vector<unsigned>& values,
 			position.x *= tileSize.x;
 			position.y *= tileSize.y;
 			tile.setPosition(position);
-			// TODO: Scale tile? (scale funtion paramiter)
 			mTiles.push_back(tile);
 		}
 	}
 }
 
-std::size_t ph::Map::findTilesetIndex(unsigned value, const TilesetsData& tilesets) const
+std::size_t ph::Map::findTilesetIndex(unsigned globalTileId, const TilesetsData& tilesets) const
 {
-	for (std::size_t i = 0; i < tilesets.globalIds.size(); ++i) {
-		const unsigned lastGlobalTileIdInCurrentTileset = tilesets.globalIds[i] + tilesets.tileCounts[i] - 1;
-		if (value >= tilesets.globalIds[i] && value <= lastGlobalTileIdInCurrentTileset) 
+	for (std::size_t i = 0; i < tilesets.firstGlobalTileIds.size(); ++i) {
+		const unsigned firstGlobalTileId = tilesets.firstGlobalTileIds[i];
+		const unsigned lastGlobalTileId = tilesets.firstGlobalTileIds[i] + tilesets.tileCounts[i] - 1;
+		if (globalTileId >= firstGlobalTileId && globalTileId <= lastGlobalTileId)
 			return i;
 	}
 	return std::string::npos;
