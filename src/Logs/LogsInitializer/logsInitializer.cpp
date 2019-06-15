319a60f8fc7e639a587c7b33d43f72dc4d4ff000
Czapa <gesio888lol@wp.pl>
2019-06-15 22:37:34 +0200

    refactor in LogsInitializer

--- src/Logs/LogsInitializer/logsInitializer.cpp
+++ src/Logs/LogsInitializer/logsInitializer.cpp
@@ -3,40 +3,42 @@
 
 bool ph::LogsInitializer::getShouldLogIntoConsole()
 {
-	openTheFile();
-	if (findPhrase("ShouldBeWrittenIntoConsole="))
-	{
-		bool returnVal = getBool(currentLine);
-		closeTheFile();
-		return returnVal;
-	}
+	return getShouldLogInto(LogOutputTarget::console);
 }
 
 bool ph::LogsInitializer::getShouldLogIntoFile()
 {
+	return getShouldLogInto(LogOutputTarget::file);
+}
+
+bool ph::LogsInitializer::getShouldLogInto(LogOutputTarget target)
+{
 	openTheFile();
-	if (findPhrase("ShouldBeWrittenIntoFile="))
-	{
+	std::string phrase = "ShouldBeWrittenInto";
+	if(target == LogOutputTarget::console)
+		phrase += "Console=";
+	else if(target == LogOutputTarget::file)
+		phrase += "File=";
+	if(findPhrase(phrase)) {
 		bool returnVal = getBool(currentLine);
 		closeTheFile();
 		return returnVal;
 	}
+	closeTheFile();
 }
 
 std::vector<ph::LogType> ph::LogsInitializer::getLogTypesToWrite()
 {
 	openTheFile();
 	std::vector<LogType> InitLogTypesToWrite;
-	if (findPhrase("LogTypesToWrite="))
-	{
-		if (findValue("All"))
-			InitLogTypesToWrite = { LogType::Warning, LogType::Error, LogType::Info, LogType::FromUser, LogType::Count };
-		else
-		{
-			if (findValue("Error")) InitLogTypesToWrite.emplace_back(LogType::Error);
-			if (findValue("Info")) InitLogTypesToWrite.emplace_back(LogType::Info);
-			if (findValue("Warning")) InitLogTypesToWrite.emplace_back(LogType::Warning);
-			if (findValue("From user")) InitLogTypesToWrite.emplace_back(LogType::FromUser);
+	if(findPhrase("LogTypesToWrite=")) {
+		if(findValue("All"))
+			InitLogTypesToWrite = {LogType::Warning, LogType::Error, LogType::Info, LogType::FromUser, LogType::Count};
+		else{
+			if(findValue("Error")) InitLogTypesToWrite.emplace_back(LogType::Error);
+			if(findValue("Info")) InitLogTypesToWrite.emplace_back(LogType::Info);
+			if(findValue("Warning")) InitLogTypesToWrite.emplace_back(LogType::Warning);
+			if(findValue("From user")) InitLogTypesToWrite.emplace_back(LogType::FromUser);
 		}
 	}
 	closeTheFile();
@@ -47,25 +49,25 @@ std::vector<std::string> ph::LogsInitializer::getModuleNamesToWrite()
 {
 	openTheFile();
 	std::vector<std::string> InitModuleNamesToWrite;
-	if (findPhrase("ModuleNamesToWrite="))
+	if(findPhrase("ModuleNamesToWrite="))
 	{
-		if (findValue("All"))
-			InitModuleNamesToWrite = { "Audio", "Base", "Input", "Logs", "Physics",
-			"Renderer", "Resources", "States", "Utilities", "World", "Terminal", "None" };
-		else
-		{
-			if (findValue("Audio")) InitModuleNamesToWrite.emplace_back("Audio");
-			if (findValue("Base")) InitModuleNamesToWrite.emplace_back("Base");
-			if (findValue("Input")) InitModuleNamesToWrite.emplace_back("Input");
-			if (findValue("Logs")) InitModuleNamesToWrite.emplace_back("Logs");
-			if (findValue("Physics")) InitModuleNamesToWrite.emplace_back("Physics");
-			if (findValue("Renderer")) InitModuleNamesToWrite.emplace_back("Renderer");
-			if (findValue("Resources")) InitModuleNamesToWrite.emplace_back("Resources");
-			if (findValue("States")) InitModuleNamesToWrite.emplace_back("States");
-			if (findValue("Utilities")) InitModuleNamesToWrite.emplace_back("Utilities");
-			if (findValue("World")) InitModuleNamesToWrite.emplace_back("World");
-			if (findValue("Terminal")) InitModuleNamesToWrite.emplace_back("Terminal");
-			if (findValue("None")) InitModuleNamesToWrite.emplace_back("None");
+		if(findValue("All")) {
+			InitModuleNamesToWrite = {"Audio", "Base", "Input", "Logs", "Physics",
+			"Renderer", "Resources", "States", "Utilities", "World", "Terminal", "None"};
+		}
+		else{
+			if(findValue("Audio")) InitModuleNamesToWrite.emplace_back("Audio");
+			if(findValue("Base")) InitModuleNamesToWrite.emplace_back("Base");
+			if(findValue("Input")) InitModuleNamesToWrite.emplace_back("Input");
+			if(findValue("Logs")) InitModuleNamesToWrite.emplace_back("Logs");
+			if(findValue("Physics")) InitModuleNamesToWrite.emplace_back("Physics");
+			if(findValue("Renderer")) InitModuleNamesToWrite.emplace_back("Renderer");
+			if(findValue("Resources")) InitModuleNamesToWrite.emplace_back("Resources");
+			if(findValue("States")) InitModuleNamesToWrite.emplace_back("States");
+			if(findValue("Utilities")) InitModuleNamesToWrite.emplace_back("Utilities");
+			if(findValue("World")) InitModuleNamesToWrite.emplace_back("World");
+			if(findValue("Terminal")) InitModuleNamesToWrite.emplace_back("Terminal");
+			if(findValue("None")) InitModuleNamesToWrite.emplace_back("None");
 		}
 	}
 	closeTheFile();
--- src/Logs/LogsInitializer/logsInitializer.hpp
+++ src/Logs/LogsInitializer/logsInitializer.hpp
@@ -9,10 +9,14 @@ namespace ph {
 class LogsInitializer : private IniLoader
 {
 public:
-	static bool getShouldLogIntoConsole();
-	static bool getShouldLogIntoFile();
 	static std::vector<LogType> getLogTypesToWrite();
 	static std::vector<std::string> getModuleNamesToWrite();
+
+	static bool getShouldLogIntoConsole();
+	static bool getShouldLogIntoFile();
+private:
+	enum class LogOutputTarget{ console, file };
+	static bool getShouldLogInto(LogOutputTarget);
 };
 
 }
