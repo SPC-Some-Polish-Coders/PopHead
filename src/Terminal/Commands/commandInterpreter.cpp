098f268ce64ceded08cef0ed3070bc93a11aa83e
Szymon Strugala <szymonstrugala23@gmail.com>
2019-06-17 18:23:13 +0200

    Added part of logs in 'CommandInterpreter'

--- src/Terminal/Commands/commandInterpreter.cpp
+++ src/Terminal/Commands/commandInterpreter.cpp
@@ -108,18 +108,36 @@ void ph::CommandInterpreter::turnOnOrTurnOffCollisionDebug()
 
 	//if(commandContains("off")) must be first! Do not change the order!
 	if (commandContains("off"))
+	{
 		collisionDebugSettings.turnOff();
+		PH_LOG(LogType::Info, "Collision debug mode is turned off.");
+	}
 	else if (commandContains("on"))
+	{
 		collisionDebugSettings.turnOn();
+		PH_LOG(LogType::Info, "Collision debug mode is turned on.");
+	}
 }
 
 void ph::CommandInterpreter::changeCollisionDebugColor()
 {
 	auto& collisionDebugSettings = CollisionDebugSettings::getInstance();
 
-	if(commandContains('1'))		collisionDebugSettings.setColors(1);
-	else if(commandContains('2'))	collisionDebugSettings.setColors(2);
-	else if(commandContains('3'))	collisionDebugSettings.setColors(3);
+	if (commandContains('1'))
+	{ 
+		collisionDebugSettings.setColors(1);
+		PH_LOG(LogType::Info, "Collision debug color mode is set to 1.");
+	}
+	else if (commandContains('2'))
+	{
+		collisionDebugSettings.setColors(2);
+		PH_LOG(LogType::Info, "Collision debug color mode is set to 2.");
+	}
+	else if(commandContains('3'))	
+	{
+		collisionDebugSettings.setColors(3);
+		PH_LOG(LogType::Info, "Collision debug color mode is set to 3.");
+	}
 	else
 		PH_LOG(LogType::Error, "Incorrect second argument! You can set collision debug color only from 1 to 3.");
 }
@@ -128,28 +146,52 @@ void ph::CommandInterpreter::changeCollisionDebugDisplayMode()
 {
 	auto& collisionDebugSettings = CollisionDebugSettings::getInstance();
 
-	if (commandContains("kinematic"))	collisionDebugSettings.displayOnlyKinematicBodies();
-	else if (commandContains("static"))	collisionDebugSettings.displayOnlyStaticBodies();
-	else if (commandContains("all"))	collisionDebugSettings.displayAllBodies();
+	if (commandContains("kinematic"))
+	{
+		collisionDebugSettings.displayOnlyKinematicBodies();
+		PH_LOG(LogType::Info, "Collition debug display mode is set to kinematic bodies.");
+	}
+	else if (commandContains("static"))
+	{
+		collisionDebugSettings.displayOnlyStaticBodies();
+		PH_LOG(LogType::Info, "Collition debug display mode is set to static bodies.");
+	}
+	else if (commandContains("all"))
+	{
+		collisionDebugSettings.displayAllBodies();
+		PH_LOG(LogType::Info, "Collition debug display mode is set to all bodies.");
+	}
 	else	PH_LOG(LogType::Error, "Incorrect second argument! Display mode not found.");
 }
 
 void ph::CommandInterpreter::executeMute()
 {
-	if(commandContains("music") || commandContains("all"))
+	if (commandContains("music") || commandContains("all"))
+	{
 		mGameData->getMusicPlayer().setMuted(true);
-	else if(commandContains("sound") || commandContains("all"))
+		PH_LOG(LogType::Info, "Muted music.");
+	}
+	else if (commandContains("sound") || commandContains("all"))
+	{
 		mGameData->getSoundPlayer().setMuted(true);
+		PH_LOG(LogType::Info, "Muted sounds.");
+	}
 	else 
 		PH_LOG(LogType::Error, "Incorrect second argument! Specified module not found.");
 }
 
 void ph::CommandInterpreter::executeUnmute()
 {
-	if(commandContains("music") || commandContains("all"))
+	if (commandContains("music") || commandContains("all"))
+	{
 		mGameData->getMusicPlayer().setMuted(false);
-	else if(commandContains("sound") || commandContains("all"))
+		PH_LOG(LogType::Info, "Unmuted music.");
+	}
+	else if (commandContains("sound") || commandContains("all"))
+	{
 		mGameData->getSoundPlayer().setMuted(false);
+		PH_LOG(LogType::Info, "Unmuted sounds.");
+	}
 	else
 		PH_LOG(LogType::Error, "Incorrect second argument! Specified module not found.");
 }
@@ -159,12 +201,19 @@ void ph::CommandInterpreter::executeSetVolume()
 	float newVolume = getVolumeFromCommand();
 
 	if (commandContains("music"))
+	{
 		mGameData->getMusicPlayer().setVolume(newVolume);
+		PH_LOG(LogType::Info, "Music volume is set to " + std::to_string(newVolume) + ".");
+	}
 	else if (commandContains("sound"))
+	{
 		mGameData->getSoundPlayer().setVolume(newVolume);
+		PH_LOG(LogType::Info, "Sound volume is set to " + std::to_string(newVolume) + ".");
+	}
 	else{
 		mGameData->getMusicPlayer().setVolume(newVolume);
 		mGameData->getSoundPlayer().setVolume(newVolume);
+		PH_LOG(LogType::Info, "Sound and music volume is set to " + std::to_string(newVolume) + ".");
 	}
 }
 
