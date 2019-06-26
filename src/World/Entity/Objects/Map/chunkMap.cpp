71be566a9c1b8732f2c9152824e510d7bdae7d38
Czapa <gesio888lol@wp.pl>
2019-06-26 16:20:21 +0200

    New chunk system doesn't crash the application, but chunk aren't drawn for some reason.

--- src/World/Entity/Objects/Map/chunkMap.cpp
+++ src/World/Entity/Objects/Map/chunkMap.cpp
@@ -19,21 +19,28 @@ ChunkMap::ChunkMap(const sf::Vector2u mapSizeInTiles, const sf::Texture& tileset
 		mChunks.emplace_back(rowOfChunks);
 }
 
-inline bool ChunkMap::doesMapNotFitInChunksOnXAxis(const sf::Vector2u mapSizeInTiles)
+bool ChunkMap::doesMapNotFitInChunksOnXAxis(const sf::Vector2u mapSizeInTiles)
 {
 	return mapSizeInTiles.x % mChunkSize.x != 0;
 }
 
-inline bool ChunkMap::doesMapNotFitInChunksOnYAxis(const sf::Vector2u mapSizeInTiles)
+bool ChunkMap::doesMapNotFitInChunksOnYAxis(const sf::Vector2u mapSizeInTiles)
 {
 	return mapSizeInTiles.y % mChunkSize.y != 0;
 }
 
 void ChunkMap::addTile(const Tile& tile)
 {
-	const int chunkIndexX = mMapSizeInTiles.x / tile.mTopLeftCornerPositionInWorld.x == 0 ? 1 : tile.mTopLeftCornerPositionInWorld.x;
-	const int chunkIndexY = mMapSizeInTiles.y / tile.mTopLeftCornerPositionInWorld.y == 0 ? 1 : tile.mTopLeftCornerPositionInWorld.y;
-	mChunks[chunkIndexX][chunkIndexY].addTile(tile);
+	const sf::Vector2u chunkPosition = getChunkPositionInVectorOfChunksToWhichNewTileShouldBeAdded(tile);
+	mChunks[chunkPosition.x][chunkPosition.y].addTile(tile);
+}
+
+sf::Vector2u ChunkMap::getChunkPositionInVectorOfChunksToWhichNewTileShouldBeAdded(const Tile& tile)
+{
+	return sf::Vector2u(
+		mMapSizeInTiles.x / (tile.mTopLeftCornerPositionInWorld.x == 0 ? 1 : tile.mTopLeftCornerPositionInWorld.x / mTileSize.x) / mChunkSize.x,
+		mMapSizeInTiles.y / (tile.mTopLeftCornerPositionInWorld.y == 0 ? 1 : tile.mTopLeftCornerPositionInWorld.y / mTileSize.y) / mChunkSize.y
+	);
 }
 
 void ChunkMap::draw(sf::RenderTarget& target, const sf::RenderStates states)
--- src/World/Entity/Objects/Map/chunkMap.hpp
+++ src/World/Entity/Objects/Map/chunkMap.hpp
@@ -10,10 +10,11 @@ class ChunkMap
 {
 public:
 	ChunkMap(const sf::Vector2u mapSizeInTiles, const sf::Texture& tileset);
-	inline bool doesMapNotFitInChunksOnXAxis(const sf::Vector2u mapSizeInTiles);
-	inline bool doesMapNotFitInChunksOnYAxis(const sf::Vector2u mapSizeInTiles);
-	
+	bool doesMapNotFitInChunksOnXAxis(const sf::Vector2u mapSizeInTiles);
+	bool doesMapNotFitInChunksOnYAxis(const sf::Vector2u mapSizeInTiles);
+
 	void addTile(const Tile&);
+	sf::Vector2u getChunkPositionInVectorOfChunksToWhichNewTileShouldBeAdded(const Tile&);
 
 	void draw(sf::RenderTarget&, const sf::RenderStates);
 
@@ -21,6 +22,7 @@ private:
 	std::vector<std::vector<Chunk>> mChunks;
 	sf::Vector2u mMapSizeInTiles;
 	const sf::Vector2u mChunkSize = sf::Vector2u(24, 24);
+	const sf::Vector2f mTileSize = sf::Vector2f(16.f, 16.f);
 	//TODO: Move this constant somewhere where Chunk can use it as well. Maybe separate file (mapConstants.hpp)
 };
 
