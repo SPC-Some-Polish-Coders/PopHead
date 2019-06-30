303cb9c72d233f62f4367dad2a73f44249e9d0b2
Piotr Gardocki <pgardocki@interia.pl>
2019-06-30 22:11:58 +0200

    Add one initial test

---
+++ tests/Resources/testResourceHolder.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+
+#include "Resources/resourceHolder.hpp"
+
+TEST_CASE("Initial test - just creating TextureHolder", "[Resources][ResourceHolder][TextureHolder]")
+{
+	ph::TextureHolder textures;
+}
