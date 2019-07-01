8245c461bb80eddd6f62c0a00aad1a7fd0c291a9
Piotr Gardocki <pgardocki@interia.pl>
2019-07-01 22:44:21 +0200

    Added ConsoleHandler for logs

---
+++ src/Logs/ConcreteHandlers/consoleHandler.cpp
@@ -0,0 +1,16 @@
+#include "consoleHandler.hpp"
+
+#include <iostream>
+#include <iomanip>
+
+namespace ph {
+
+	void ConsoleHandler::utilizeLog(const LogRecord& logRecord)
+	{
+		std::cout << "[  " << std::left << std::setw(7)
+				  << std::to_string(logRecord.secondsFromStart).erase(5, 4) << "s ]"
+				  << " | " << std::setw(9) << std::left << logTypeToString(logRecord.type)
+				  << " | " << std::setw(9) << std::left << logRecord.moduleName
+				  << " | " << std::left << logRecord.message << std::endl;
+	}
+}
