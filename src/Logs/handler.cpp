a54965c229ac959e1a241efda56aff91bddfe2aa
Piotr Gardocki <pgardocki@interia.pl>
2019-07-02 22:39:52 +0200

    Add basic filterring to log handlers

--- src/Logs/handler.cpp
+++ src/Logs/handler.cpp
@@ -1,11 +1,60 @@
 #include "handler.hpp"
 
+#include <algorithm>
+
 namespace ph {
+	
+	Handler::Handler()
+	{
+		allowedModules = {
+			{"Audio", true},
+			{"EfficiencyRegister", true},
+			{"Gui", true},
+			{"Input", true},
+			{"Logs", true},
+			{"Physics", true},
+			{"Renderer", true},
+			{"Resources", true},
+			{"States", true},
+			{"Terminal", true},
+			{"Utilities", true},
+			{"World", true}
+		};
+
+		allowedTypes = {
+			{LogType::Info, true},
+			{LogType::Error, true},
+			{LogType::FromUser, true},
+			{LogType::Warning, true},
+			{LogType::Exception, true},
+			{LogType::UnhandledException, true}
+		};
+	}
 
 	void Handler::handleLog(const LogRecord& logRecord)
 	{
 		if (isPassedByFilter(logRecord))
 			utilizeLog(logRecord);
+	}
+
+	void Handler::setModuleAllowing(const std::string& moduleName, bool allowed)
+	{
+		auto iter = std::find_if(allowedModules.begin(), allowedModules.end(), [&moduleName](const std::pair<std::string, bool>& pair) {
+			return pair.first == moduleName;
+			});
+
+		if (iter != allowedModules.end())
+			iter->second = allowed;
+	}
+
+	void Handler::setTypeAllowing(LogType type, bool allowed)
+	{
+		auto iter = std::find_if(allowedTypes.begin(), allowedTypes.end(), [&type](const std::pair<LogType, bool>& pair) {
+			return pair.first == type;
+			});
+
+		if (iter != allowedTypes.end())
+			iter->second = allowed;
 	}
 
 	bool Handler::isPassedByFilter(const LogRecord& logRecord)
--- src/Logs/handler.hpp
+++ src/Logs/handler.hpp
@@ -3,17 +3,27 @@
 #include "logRecord.hpp"
 
 #include <string>
+#include <vector>
+#include <utility>
 
 namespace ph {
 	
 	class Handler
 	{
 	public:
+		Handler();
+
 		void handleLog(const LogRecord& logRecord);
+
+		void setModuleAllowing(const std::string& moduleName, bool allowed);
+		void setTypeAllowing(LogType type, bool allowed);
 
 	private:
 		virtual void utilizeLog(const LogRecord& logRecord) = 0;
 
 		bool isPassedByFilter(const LogRecord& logRecord);
+
+		std::vector<std::pair<std::string, bool>> allowedModules;
+		std::vector<std::pair<LogType, bool>> allowedTypes;
 	};
 }
