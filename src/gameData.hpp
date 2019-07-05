d60733b252d68fb0cf6e8b184feb5eb155c0fe11
Czapa <gesio888lol@wp.pl>
2019-07-05 21:55:10 +0200

    refactor in GameData

--- src/gameData.hpp
+++ src/gameData.hpp
@@ -20,33 +20,50 @@ namespace ph {
 class GameData
 {
 public:
-	inline GameData();
-	inline GameData(
-		SoundPlayer* const,
-		MusicPlayer* const,
-		TextureHolder* const,
-		FontHolder* const,
-		ShaderHolder* const,
-		StateMachine* const,
-		Input* const,
-		Renderer* const,
-		PhysicsEngine* const,
-		Terminal* const,
-		EfficiencyRegister* const,
-		GUI* const);
-
-	auto getSoundPlayer() const -> SoundPlayer & { return *mSoundPlayer; }
-	auto getMusicPlayer() const -> MusicPlayer & { return *mMusicPlayer; }
-	auto getTextures() const -> TextureHolder & { return *mTextures; }
-	auto getFonts()	const -> FontHolder & { return *mFonts; }
-	auto getShaders() const -> ShaderHolder & { return *mShaders; }
-	auto getStateMachine() const -> StateMachine & { return *mStateMachine; }
-	auto getInput()	const -> Input & { return *mInput; }
-	auto getRenderer() const -> Renderer & { return *mRenderer; }
-	auto getPhysicsEngine()	const -> PhysicsEngine & { return *mPhysicsEngine; }
-	auto getTerminal() const -> Terminal & { return *mTerminal; }
-	auto getEfficiencyRegister() const -> EfficiencyRegister & { return *mEfficiencyRegister; }
-	auto getGui() const -> GUI & { return *mGui; }
+	GameData()
+	:GameData(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr) {}
+	
+	GameData(
+		SoundPlayer* const soundPlayer,
+		MusicPlayer* const musicPlayer,
+		TextureHolder* const textures,
+		FontHolder* const fonts,
+		ShaderHolder* const shaders,
+		StateMachine* const stateMachine,
+		Input* const input,
+		Renderer* const renderer,
+		PhysicsEngine* const physicsEngine,
+		Terminal* const Terminal,
+		EfficiencyRegister* const efficiencyRegister,
+		GUI* const Gui
+	)
+		:mSoundPlayer{soundPlayer}
+		,mMusicPlayer{musicPlayer}
+		,mTextures{textures}
+		,mFonts{fonts}
+		,mShaders{shaders}
+		,mStateMachine{stateMachine}
+		,mInput{input}
+		,mRenderer{renderer}
+		,mPhysicsEngine{physicsEngine}
+		,mTerminal{Terminal}
+		,mEfficiencyRegister{efficiencyRegister}
+		,mGui(Gui)
+	{
+	}
+	
+	auto getSoundPlayer() const -> SoundPlayer& { return *mSoundPlayer; }
+	auto getMusicPlayer() const -> MusicPlayer& { return *mMusicPlayer; }
+	auto getTextures() const -> TextureHolder& { return *mTextures; }
+	auto getFonts()	const -> FontHolder& { return *mFonts; }
+	auto getShaders() const -> ShaderHolder& { return *mShaders; }
+	auto getStateMachine() const -> StateMachine& { return *mStateMachine; }
+	auto getInput()	const -> Input& { return *mInput; }
+	auto getRenderer() const -> Renderer& { return *mRenderer; }
+	auto getPhysicsEngine()	const -> PhysicsEngine& { return *mPhysicsEngine; }
+	auto getTerminal() const -> Terminal& { return *mTerminal; }
+	auto getEfficiencyRegister() const -> EfficiencyRegister& { return *mEfficiencyRegister; }
+	auto getGui() const -> GUI& { return *mGui; }
 
 private:
 	SoundPlayer* const mSoundPlayer;
@@ -62,37 +79,5 @@ private:
 	EfficiencyRegister* const mEfficiencyRegister;
 	GUI* const mGui;
 };
-
-inline GameData::GameData()
-	:GameData(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr) {}
-
-inline GameData::GameData(
-	SoundPlayer* const soundPlayer,
-	MusicPlayer* const musicPlayer,
-	TextureHolder* const textures,
-	FontHolder* const fonts,
-	ShaderHolder* const shaders,
-	StateMachine* const stateMachine,
-	Input* const input,
-	Renderer* const renderer,
-	PhysicsEngine* const physicsEngine,
-	Terminal* const Terminal,
-	EfficiencyRegister* const efficiencyRegister,
-	GUI* const Gui
-)
-	:mSoundPlayer{soundPlayer}
-	,mMusicPlayer{musicPlayer}
-	,mTextures{textures}
-	,mFonts{fonts}
-	,mShaders{shaders}
-	,mStateMachine{stateMachine}
-	,mInput{input}
-	,mRenderer{renderer}
-	,mPhysicsEngine{physicsEngine}
-	,mTerminal{Terminal}
-	,mEfficiencyRegister{efficiencyRegister}
-	,mGui(Gui)
-{
-}
 
 }
