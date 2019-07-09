b205900416c42c543c8eaa1035a3e20ee567b903
Czapa <gesio888lol@wp.pl>
2019-07-09 19:01:23 +0200

    refactor in testStaticCollisionHandler.cpp

--- tests/CollisionHandlers/testStaticCollisionHandler.cpp
+++ tests/CollisionHandlers/testStaticCollisionHandler.cpp
@@ -7,36 +7,35 @@
 
 namespace ph {
 
-TEST_CASE("When kinematic body and static body collide with each other, kinematic body is sticked to appropriate static body's bound",
-          "[Physics][StaticCollisionHandler]") 
+TEST_CASE("static collision after straight movement", "[Physics][StaticCollisionHandler]") 
 {
 	StaticCollisionHandler staticCollisionHandler;
 	
 	CollisionBody kinematicBody({0, 0, 10, 10}, 25);
 	kinematicBody.actionsAtTheEndOfPhysicsLoopIteration();
 
-	SECTION("when kinematic body is colliding with left bound of static body") {
+	SECTION("after moving right") {
 		kinematicBody.move({10, 0});
 		CollisionBody staticBody({15, -10, 20, 30}, 0);
 		staticCollisionHandler(kinematicBody, staticBody);
 		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 5.f);
 		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 0.f);
 	}
-	SECTION("when kinematic body is colliding with right bound of static body") {
+	SECTION("after moving left") {
 		kinematicBody.move({-10, 0});
 		CollisionBody staticBody({-25, -10, 20, 30}, 0);
 		staticCollisionHandler(kinematicBody, staticBody);
 		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == -5.f);
 		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 0.f);
 	}
-	SECTION("when kinematic body is colliding with top bound of static body") {
+	SECTION("after moving down") {
 		kinematicBody.move({0, 10});
 		CollisionBody staticBody({-10, 15, 30, 20}, 0);
 		staticCollisionHandler(kinematicBody, staticBody);
 		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 0.f);
 		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 5.f);
 	}
-	SECTION("when kinematic body is colliding with bottom bound of static body") {
+	SECTION("after moving up") {
 		kinematicBody.move({0, -10});
 		CollisionBody staticBody({-10, -25, 30, 20}, 0);
 		staticCollisionHandler(kinematicBody, staticBody);
