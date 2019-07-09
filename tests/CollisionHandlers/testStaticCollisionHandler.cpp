7f6933c744928748c5f983eef99090db7fbcad92
Czapa <gesio888lol@wp.pl>
2019-07-09 20:02:07 +0200

    Added tests of static collision after diagonal movement to testStaticCollisionHandler.cpp

--- tests/CollisionHandlers/testStaticCollisionHandler.cpp
+++ tests/CollisionHandlers/testStaticCollisionHandler.cpp
@@ -7,7 +7,7 @@
 
 namespace ph {
 
-TEST_CASE("static collision after straight movement", "[Physics][StaticCollisionHandler]") 
+TEST_CASE("static collision is properly handled", "[Physics][StaticCollisionHandler]") 
 {
 	StaticCollisionHandler staticCollisionHandler;
 	
@@ -40,6 +40,66 @@ TEST_CASE("static collision after straight movement", "[Physics][StaticCollision
 		CollisionBody staticBody({-10, -25, 30, 20}, 0);
 		staticCollisionHandler(kinematicBody, staticBody);
 		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 0.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == -5.f);
+	}
+
+	SECTION("after moving diagonally down right (wall is to the right from kinematic body)") {
+		kinematicBody.move({10, 10});
+		CollisionBody staticBody({15, -10, 20, 30}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 5.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 10.f);
+	}
+	SECTION("after moving diagonally up right (wall is to the right from kinematic body)") {
+		kinematicBody.move({10, -10});
+		CollisionBody staticBody({15, -10, 20, 30}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 5.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == -10.f);
+	}
+
+	SECTION("after moving diagonally down left (wall is to the left from kinematic body)") {
+		kinematicBody.move({-10, 10});
+		CollisionBody staticBody({-25, -10, 20, 30}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == -5.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 10.f);
+	}
+	SECTION("after moving diagonally up left (wall is to the left from kinematic body)") {
+		kinematicBody.move({-10, -10});
+		CollisionBody staticBody({-25, -10, 20, 30}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == -5.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == -10.f);
+	}
+
+	SECTION("after moving diagonally down left (wall is down from kinematic body)") {
+		kinematicBody.move({-10, 10});
+		CollisionBody staticBody({-10, 15, 30, 20}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == -10.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 5.f);
+	}
+	SECTION("after moving diagonally down right (wall is down from kinematic body)") {
+		kinematicBody.move({10, 10});
+		CollisionBody staticBody({-10, 15, 30, 20}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 10.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == 5.f);
+	}
+	
+	SECTION("after moving diagonally up left (wall is up from kinematic body)") {
+		kinematicBody.move({-10, -10});
+		CollisionBody staticBody({-10, -25, 30, 20}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == -10.f);
+		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == -5.f);
+	}
+	SECTION("after moving diagonally up right (wall is up from kinematic body)") {
+		kinematicBody.move({10, -10});
+		CollisionBody staticBody({-10, -25, 30, 20}, 0);
+		staticCollisionHandler(kinematicBody, staticBody);
+		CHECK(Approx(kinematicBody.getPosition().x).margin(0.001f) == 10.f);
 		CHECK(Approx(kinematicBody.getPosition().y).margin(0.001f) == -5.f);
 	}
 }
