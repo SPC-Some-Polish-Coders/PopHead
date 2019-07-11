e441cc97f4840df6d53358a1991e96002ea9c54a
Czapa <gesio888lol@wp.pl>
2019-07-11 18:34:18 +0200

    refactor and optimalization in Math namespace

--- src/Utilities/math.hpp
+++ src/Utilities/math.hpp
@@ -6,28 +6,28 @@
 namespace ph {
 
 namespace Math {
-	inline float getRightBound(sf::FloatRect bounds)
+	inline float getRightBound(const sf::FloatRect& bounds)
 	{ return bounds.left + bounds.width; }
 
-	inline float getBottomBound(sf::FloatRect bounds)  
+	inline float getBottomBound(const sf::FloatRect& bounds)  
 	{ return bounds.top + bounds.height; }
 
-	inline sf::Vector2f getCenter(sf::FloatRect bounds)
+	inline sf::Vector2f getCenter(const sf::FloatRect& bounds)
 	{ return sf::Vector2f(bounds.left + bounds.width / 2.f, bounds.top + bounds.height / 2.f); }
 
 	inline sf::Vector2f getTopLeftCorner(const sf::FloatRect& bounds)
 	{ return sf::Vector2f(bounds.left, bounds.top); }
 
-	inline sf::Vector2f getTopRightCorner(sf::FloatRect bounds)
+	inline sf::Vector2f getTopRightCorner(const sf::FloatRect& bounds)
 	{ return sf::Vector2f(getRightBound(bounds), bounds.top); }
 
-	inline sf::Vector2f getBottomLeftCorner(sf::FloatRect bounds)
+	inline sf::Vector2f getBottomLeftCorner(const sf::FloatRect& bounds)
 	{ return sf::Vector2f(bounds.left, getBottomBound(bounds)); }
 
-	inline sf::Vector2f getBottomRightCorner(sf::FloatRect bounds)
+	inline sf::Vector2f getBottomRightCorner(const sf::FloatRect& bounds)
 	{ return sf::Vector2f(getRightBound(bounds), getBottomBound(bounds)); }
 
-	inline sf::Vector2u getTwoDimensionalPositionFromOneDimensionalArrayIndex(unsigned index, unsigned numberOfColumns)
+	inline sf::Vector2u getTwoDimensionalPositionFromOneDimensionalArrayIndex(const unsigned index, const unsigned numberOfColumns)
 	{
 		PH_ASSERT_EXCEPTION(numberOfColumns != 0, "Number of columns cannot be 0");
 		return sf::Vector2u(index % numberOfColumns, index / numberOfColumns); 
@@ -43,7 +43,7 @@ namespace Math {
 		getBottomBound(A) > B.top;
 	}
 
-	inline sf::Vector2f lerp(sf::Vector2f source, sf::Vector2f destination, float speed)
+	inline sf::Vector2f lerp(const sf::Vector2f source, const sf::Vector2f destination, const float speed)
 	{
 		PH_ASSERT(speed >= 0.f, "Speed cannot be less than 0");
 		return source + (destination - source) * speed;
