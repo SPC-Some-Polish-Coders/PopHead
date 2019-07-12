b72b892d26a90ea3fda9938fb06ba02b96984094
huberti1 <01huberti@gmail.com>
2019-07-12 21:11:13 +0200

    Added support for all whitespace characters to Xml.

--- src/Utilities/xml.hpp
+++ src/Utilities/xml.hpp
@@ -42,6 +42,7 @@ private:
 	std::size_t findEndOfTagAttributes(std::size_t offset = 0) const
 	{ return mContent.find('>', offset); }
 
+	inline static const std::string whitespaceCharacters = " \n\t\v\f\r";
 	std::string mContent;
 };
 
