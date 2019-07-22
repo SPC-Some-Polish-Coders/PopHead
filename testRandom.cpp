5db6eaef686c84377a2e2baa669535d3057001dd
Szymon Strugala <szymonstrugala23@gmail.com>
2019-07-22 08:38:35 +0200

    replace REQUIRE macro with CHECK macro in testRandom.cpp

--- testRandom.cpp
+++ testRandom.cpp
@@ -11,25 +11,25 @@ TEST_CASE("is a generated number from given range", "[Utilities][Random]")
 		SECTION("small range")
 		{
 			float generatedPositiveNumber = Random::generateNumber(2.f, 4.f);
-			REQUIRE(generatedPositiveNumber >= 2.f);
-			REQUIRE(generatedPositiveNumber < 4.f);
+			CHECK(generatedPositiveNumber >= 2.f);
+			CHECK(generatedPositiveNumber < 4.f);
 		}
 		SECTION("standard range")
 		{
 			float generatedPositiveNumber = Random::generateNumber(0.f, 20.f);
-			REQUIRE(generatedPositiveNumber >= 0.f);
-			REQUIRE(generatedPositiveNumber < 20.f);
+			CHECK(generatedPositiveNumber >= 0.f);
+			CHECK(generatedPositiveNumber < 20.f);
 		}
 		SECTION("big range")
 		{
 			float generatedPositiveNumber = Random::generateNumber(1000.f, 20000.f);
-			REQUIRE(generatedPositiveNumber >= 1000.f);
-			REQUIRE(generatedPositiveNumber < 20000.f);
+			CHECK(generatedPositiveNumber >= 1000.f);
+			CHECK(generatedPositiveNumber < 20000.f);
 		}
 		SECTION("limited range")
 		{
 			float generatedPositiveNumber = Random::generateNumber(1.f, 1.f);
-			REQUIRE(generatedPositiveNumber == 1.f);
+			CHECK(generatedPositiveNumber == 1.f);
 		}
 	}
 	SECTION("negative numbers")
@@ -37,25 +37,25 @@ TEST_CASE("is a generated number from given range", "[Utilities][Random]")
 		SECTION("small range")
 		{
 			float generatedNegativeNumber = Random::generateNumber(-4.f, -2.f);
-			REQUIRE(generatedNegativeNumber >= -4.f);
-			REQUIRE(generatedNegativeNumber < -2.f);
+			CHECK(generatedNegativeNumber >= -4.f);
+			CHECK(generatedNegativeNumber < -2.f);
 		}
 		SECTION("standard range")
 		{
 			float generatedNegativeNumber = Random::generateNumber(-20.f, -10.f);
-			REQUIRE(generatedNegativeNumber >= -20.f);
-			REQUIRE(generatedNegativeNumber < -10.f);
+			CHECK(generatedNegativeNumber >= -20.f);
+			CHECK(generatedNegativeNumber < -10.f);
 		}
 		SECTION("big range")
 		{
 			float generatedNegativeNumber = Random::generateNumber(-20.f, -10.f);
-			REQUIRE(generatedNegativeNumber >= -20.f);
-			REQUIRE(generatedNegativeNumber < -10.f);
+			CHECK(generatedNegativeNumber >= -20.f);
+			CHECK(generatedNegativeNumber < -10.f);
 		}
 		SECTION("limited range")
 		{
 			float generatedNegativeNumber = Random::generateNumber(-1.f, -1.f);
-			REQUIRE(generatedNegativeNumber == -1.f);
+			CHECK(generatedNegativeNumber == -1.f);
 		}
 	}
 	SECTION("real numbers")
@@ -63,20 +63,20 @@ TEST_CASE("is a generated number from given range", "[Utilities][Random]")
 		SECTION("small range")
 		{
 			float generatedRealNumber = Random::generateNumber(-2.f, 2.f);
-			REQUIRE(generatedRealNumber >= -2.f);
-			REQUIRE(generatedRealNumber < 2.f);
+			CHECK(generatedRealNumber >= -2.f);
+			CHECK(generatedRealNumber < 2.f);
 		}
 		SECTION("standard range")
 		{
 			float generatedRealNumber = Random::generateNumber(-20.f, 20.f);
-			REQUIRE(generatedRealNumber >= -20.f);
-			REQUIRE(generatedRealNumber < 20.f);
+			CHECK(generatedRealNumber >= -20.f);
+			CHECK(generatedRealNumber < 20.f);
 		}
 		SECTION("big range")
 		{
 			float generatedRealNumber = Random::generateNumber(-2000.f, 2000.f);
-			REQUIRE(generatedRealNumber >= -2000.f);
-			REQUIRE(generatedRealNumber < 2000.f);
+			CHECK(generatedRealNumber >= -2000.f);
+			CHECK(generatedRealNumber < 2000.f);
 		}
 	}
 }
