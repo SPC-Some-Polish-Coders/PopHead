d41ff6f7434ba71656efacca9b9d41a105ed8c0e
Szymon Strugala <szymonstrugala23@gmail.com>
2019-07-23 15:11:52 +0200

    Change variables in testPath.cpp to be relatable on all OSs

--- tests/Utilities/testPath.cpp
+++ tests/Utilities/testPath.cpp
@@ -1,26 +1,16 @@
-#include "catch.hpp"
-
 #include "Utilities/path.hpp"
+#include "catch.hpp"
+#include <string>
 
 namespace ph {
 
-namespace {
-	std::string audioModuleFilePath = "\\PopHead\\src\\Audio\\";
-	std::string inputModuleFilePath = "\\PopHead\\src\\Input\\";
-	std::string utilitiesModuleFilePath = "\\PopHead\\src\\Utilities\\";
-	std::string noneFilePath = "\\PopHead\\src\\main.cpp";
-
-	std::string terminalFilePath = "\\PopHead\\src\\Terminal\\terminal.cpp";
-	std::string rendererFilePath = "\\PopHead\\src\\Renderer\\renderer.cpp";
-	std::string musicPlayerFilePath = "\\PopHead\\src\\Audio\\Music\\musicPlayer.cpp";
-
-	std::string nonexistentFilePath = "\\PopHead\\github\\git\\repo";
-	std::string duplicatedFilePath = "\\PopHead\\src\\PopHead\\src";
-	std::string incompleteFilePath = "\\PopHead\\src\\";
-}
-
 TEST_CASE("is filepath properly converted to module name", "[Utilities][Path]")
 {
+	std::string audioModuleFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "Audio" + PH_PATH_SEPARATOR;
+	std::string inputModuleFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "Input" + PH_PATH_SEPARATOR;
+	std::string utilitiesModuleFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "Utilities" + PH_PATH_SEPARATOR;
+	std::string noneFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "main.cpp";
+
 	CHECK(Path::toModuleName(audioModuleFilePath) == "Audio");
 	CHECK(Path::toModuleName(inputModuleFilePath) == "Input");
 	CHECK(Path::toModuleName(utilitiesModuleFilePath) == "Utilities");
@@ -29,6 +19,11 @@ TEST_CASE("is filepath properly converted to module name", "[Utilities][Path]")
 
 TEST_CASE("is filepath properly converted to file name", "[Utilities][Path]")
 {
+	std::string terminalFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "Terminal" + PH_PATH_SEPARATOR + "terminal.cpp";
+	std::string rendererFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "Renderer" + PH_PATH_SEPARATOR + "renderer.cpp";
+	std::string musicPlayerFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "Audio" + PH_PATH_SEPARATOR + "Music" + 
+		PH_PATH_SEPARATOR + "musicPlayer.cpp";
+
 	CHECK(Path::toFilename(terminalFilePath) == "terminal.cpp");
 	CHECK(Path::toFilename(rendererFilePath) == "renderer.cpp");
 	CHECK(Path::toFilename(musicPlayerFilePath) == "musicPlayer.cpp");
@@ -36,6 +31,11 @@ TEST_CASE("is filepath properly converted to file name", "[Utilities][Path]")
 
 TEST_CASE("are exceptions called properly", "[Utilities][Path]")
 {
+
+	std::string nonexistentFilePath = PH_PATH_SEPARATOR+ std::string("PopHead") + PH_PATH_SEPARATOR + "github" + PH_PATH_SEPARATOR + "git" + PH_PATH_SEPARATOR;
+	std::string duplicatedFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR + "src" + PH_PATH_SEPARATOR + "PopHead" + PH_PATH_SEPARATOR + "src";
+	std::string incompleteFilePath = PH_PATH_SEPARATOR + std::string("PopHead") + PH_PATH_SEPARATOR +"src" + PH_PATH_SEPARATOR;
+
 	SECTION("lack of module") {
 		CHECK_THROWS(Path::toModuleName(nonexistentFilePath));
 	}
