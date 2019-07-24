7db8089e15baf622b605b1084b7e172b311d4960
Piotr Gardocki <pgardocki@interia.pl>
2019-07-24 13:32:22 +0200

    Create a bunch of logging macros

--- src/Logs/logs.hpp
+++ src/Logs/logs.hpp
@@ -17,8 +17,42 @@
 #define PH_LOG_INFO(message)\
 	PH_LOG(ph::LogLevel::Info, message)
 
+#define PH_LOG_WARNING(message)\
+	PH_LOG(ph::LogLevel::Warning, message)
+
+#define PH_ASSERT_WARNING(expression, message)\
+	PH_ASSERT_EXPRESSION(expression, PH_LOG_WARNING(message))
+
 #else
 
 #define PH_LOG_INFO(message)
+#define PH_LOG_WARNING(message)
+#define PH_ASSERT_WARNING(expression, message)
+
+#endif // PH_DEBUG_LOGS_ENABLED
+
+
+#define PH_LOG_ERROR(message)\
+	PH_LOG(ph::LogLevel::Error, message)
+#define PH_LOG_CRITICAL(message)\
+	{ PH_LOG(ph::LogLevel::Critical, message); throw std::runtime_error(message); }
+
+#define PH_ASSERT_ERROR(expression, message)\
+	PH_ASSERT_EXPRESSION(expression, PH_LOG_ERROR(message))
+#define PH_ASSERT_CRITICAL(expression, message)\
+	PH_ASSERT_EXPRESSION(expression, PH_LOG_CRITICAL(message))
+
+
+#ifdef PH_DEBUG_LOGS_ENABLED
+
+#define PH_UNEXPECTED_SITUATION(message)\
+	PH_LOG_CRITICAL(message)
+#define PH_ASSERT_UNEXPECTED_SITUATION(expression, message)\
+	PH_ASSERT_EXPRESSION(expression, PH_UNEXPECTED_SITUATION(message))
+
+#else
+
+#define PH_UNEXPECTED_SITUATION(message)
+#define PH_ASSERT_UNEXPECTED_SITUATION(expression, message)
 
 #endif // PH_DEBUG_LOGS_ENABLED
