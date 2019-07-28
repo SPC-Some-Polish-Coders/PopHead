c653729591b0f99b8ba768eccf9b255d7960d9d7
Szymon Strugala <szymonstrugala23@gmail.com>
2019-07-28 14:59:51 +0200

    Add unordered_map in place of ineffective 'if/else' series

--- src/Utilities/cast.hpp
+++ src/Utilities/cast.hpp
@@ -3,10 +3,17 @@
 #include "Utilities/spawn.hpp"
 #include "SFML/System/Vector2.hpp"
 #include <string>
+#include <unordered_map>
 
 namespace ph {
 
 namespace Cast {
+
+	const std::unordered_map<std::string, ObjectType> objectTypeMap({
+		{ "zombie", ObjectType::Zombie },
+		{ "npc", ObjectType::Npc }
+		});
+
 	ObjectType toObjectType(const std::string& str);
 	unsigned toUnsigned(const std::string& str);
 	std::string toString(const sf::Vector2f&);
