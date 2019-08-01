32a2e2a337b782d1368cbc09b473a592e5ec643c
Czapa <gesio888lol@wp.pl>
2019-08-02 01:42:31 +0200

    Add 1st TEST_CASE for RandomPathAlgorithm

---
+++ tests/AI/testRandomPathAlgorithm.cpp
@@ -0,0 +1,47 @@
+#include <catch.hpp>
+
+#include "AI/RandomPathAlgorithm.hpp"
+#include <algorithm>
+
+namespace ph {
+
+ObstacleGrid getWalkableGrid11x11();
+
+TEST_CASE("Random path finding algorithm works properly with no obstacles", "[AI][RandomPathAlgorithm]")
+{
+	auto obstacleGrid = getWalkableGrid11x11();
+	RandomPathAlgorithm rpa(obstacleGrid);
+	Path path = rpa.getRandomPath({6, 6});
+	
+	REQUIRE((
+		( path == Path{Direction::east, Direction::east} ) ||
+		( path == Path{Direction::east, Direction::east, Direction::east} ) ||
+		( path == Path{Direction::east, Direction::east, Direction::east, Direction::east} ) ||
+		( path == Path{Direction::east, Direction::east, Direction::east, Direction::east, Direction::east} ) ||
+		( path == Path{Direction::west, Direction::west} ) ||
+		( path == Path{Direction::west, Direction::west, Direction::west}) ||
+		( path == Path{Direction::west, Direction::west, Direction::west, Direction::west}) ||
+		( path == Path{Direction::west, Direction::west, Direction::west, Direction::west, Direction::west}) ||
+		( path == Path{Direction::north, Direction::north} ) ||
+		( path == Path{Direction::north, Direction::north, Direction::north} ) ||
+		( path == Path{Direction::north, Direction::north, Direction::north, Direction::north} ) ||
+		( path == Path{Direction::north, Direction::north, Direction::north, Direction::north, Direction::north} ) ||
+		( path == Path{Direction::south, Direction::south} ) ||
+		( path == Path{Direction::south, Direction::south, Direction::south} ) ||
+		( path == Path{Direction::south, Direction::south, Direction::south, Direction::south} ) ||
+		( path == Path{Direction::south, Direction::south, Direction::south, Direction::south, Direction::south} )
+	));
+}
+
+ObstacleGrid getWalkableGrid11x11()
+{
+	std::vector<bool> column;
+	column.resize(11);
+	std::fill(column.begin(), column.end(), false);
+	ObstacleGrid grid;
+	grid.resize(11);
+	std::fill(grid.begin(), grid.end(), column);
+	return grid;
+}
+
+}
