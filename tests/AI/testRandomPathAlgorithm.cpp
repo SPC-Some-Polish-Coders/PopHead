79dce315a8aae5b877f8b3eb6d58e5b2e9489672
Czapa <gesio888lol@wp.pl>
2019-08-02 03:35:48 +0200

    Add 2nd TEST_CASE for RandomPathAlgorithm

--- tests/AI/testRandomPathAlgorithm.cpp
+++ tests/AI/testRandomPathAlgorithm.cpp
@@ -6,6 +6,7 @@
 namespace ph {
 
 ObstacleGrid getWalkableGrid11x11();
+ObstacleGrid getGridWithObstacles11x11();
 
 TEST_CASE("Random path finding algorithm works properly with no obstacles", "[AI][RandomPathAlgorithm]")
 {
@@ -41,6 +42,42 @@ ObstacleGrid getWalkableGrid11x11()
 	ObstacleGrid grid;
 	grid.resize(11);
 	std::fill(grid.begin(), grid.end(), column);
+	return grid;
+}
+
+TEST_CASE("Random path finding algorithm works properly with with obstacles", "[AI][RandomPathAlgorithm]")
+{
+	auto obstacleGrid = getGridWithObstacles11x11();
+	RandomPathAlgorithm rpa(obstacleGrid);
+	Path path = rpa.getRandomPath({6, 6});
+
+	for(int i = 0; i < 10; ++i) {
+		DYNAMIC_SECTION("iteration: "<<i) 
+		{
+			REQUIRE((
+				(path == Path{Direction::east, Direction::east}) ||
+				(path == Path{Direction::east, Direction::east, Direction::east}) ||
+				(path == Path{Direction::east, Direction::east, Direction::east, Direction::east}) ||
+				(path == Path{Direction::west, Direction::west}) ||
+				(path == Path{Direction::west, Direction::west, Direction::west}) ||
+				(path == Path{Direction::north, Direction::north})
+			));
+		}
+	}
+}
+
+ObstacleGrid getGridWithObstacles11x11()
+{
+	std::vector<bool> column;
+	column.resize(11);
+	std::fill(column.begin(), column.end(), false);
+	ObstacleGrid grid;
+	grid.resize(11);
+	std::fill(grid.begin(), grid.end(), column);
+	grid[10][5] = true;
+	grid[1][5] = true;
+	grid[5][2] = true;
+	grid[5][7] = true;
 	return grid;
 }
 
