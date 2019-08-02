768a8c85d716608d2807700dbb667be8a90856b7
Czapa <gesio888lol@wp.pl>
2019-08-02 02:43:19 +0200

    Add new Random::generateNumber() function but for integers
    I need this in RandomPathAlgorithm

--- src/Utilities/random.cpp
+++ src/Utilities/random.cpp
@@ -3,10 +3,17 @@
 
 namespace ph {
 
-float Random::generateNumber(float min, float max)
+float Random::generateNumber(const float min, const float max)
 {
 	PH_ASSERT(min <= max, "Min can't be greater than max");
 	std::uniform_real_distribution<float> dist(min, max);
+	return dist(mEngine);
+}
+
+int Random::generateNumber(const int min, const int max)
+{
+	PH_ASSERT(min <= max, "Min can't be greater than max");
+	std::uniform_int_distribution<> dist(min, max);
 	return dist(mEngine);
 }
 
--- src/Utilities/random.hpp
+++ src/Utilities/random.hpp
@@ -7,7 +7,8 @@ namespace ph {
 
 class Random {
 public:
-	static float generateNumber(float min, float max);
+	static float generateNumber(const float min, const float max);
+	static int generateNumber(const int min, const int max);
 
 private:
 	inline static std::default_random_engine mEngine = std::default_random_engine(static_cast<unsigned>(time(nullptr)));
