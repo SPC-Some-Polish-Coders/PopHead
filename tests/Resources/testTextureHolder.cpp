5d284e5ad5114e22b924454333fa0c2341bc8a02
Piotr Gardocki <pgardocki@interia.pl>
2019-08-03 11:55:33 +0200

    Add test case for TextureHolder

--- tests/Resources/testTextureHolder.cpp
+++ tests/Resources/testTextureHolder.cpp
@@ -5,7 +5,13 @@
 
 namespace ph {
 
-	TEST_CASE("Cannot get non-existing texture", "[TextureHolder][ResourceHolder]")
+	TEST_CASE("Cannot load non-existing texture", "[Resources][TextureHolder][ResourceHolder]")
+	{
+		TextureHolder textures;
+		CHECK_FALSE(textures.load("dsfgkjnsejrgkjnkjdefnkjenf"));   // make sure that this file is not real
+	}
+
+	TEST_CASE("Cannot get non-existing texture", "[Resources][TextureHolder][ResourceHolder]")
 	{
 		Tests::BufferedHandler logs;
 		logs.clearRecords();
