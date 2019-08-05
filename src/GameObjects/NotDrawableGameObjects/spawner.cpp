702a3902fd497e330c3fc6e9be365d66afb768c5
Czapa <gesio888lol@wp.pl>
2019-08-05 05:28:22 +0200

    Optimize Spawner

--- src/GameObjects/NotDrawableGameObjects/spawner.cpp
+++ src/GameObjects/NotDrawableGameObjects/spawner.cpp
@@ -1,17 +1,16 @@
-#include "GameObjects/NotDrawableGameObjects/spawner.hpp"
 #include "GameObjects/DrawableGameObjects/Characters/Enemies/zombie.hpp"
+#include "GameObjects/NotDrawableGameObjects/spawner.hpp"
 #include "Utilities/spawn.hpp"
-#include <iostream>
 
 namespace ph {
 
-Spawner::Spawner(GameData* const gameData, const std::string spawnerName,  const ObjectType objectType,
+Spawner::Spawner(GameData* const gameData, const std::string& spawnerName, const ObjectType objectType,
 		const sf::Time spawnFrequency, const sf::Vector2f position)
 	:GameObject(spawnerName)
-	,mGameData(gameData)
-	,mObjectType(objectType)
 	,mSpawnFrequency(spawnFrequency)
 	,mPosition(position)
+	,mGameData(gameData)
+	,mObjectType(objectType)
 {
 }
 
