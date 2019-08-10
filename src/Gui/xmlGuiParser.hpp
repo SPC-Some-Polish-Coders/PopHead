5d4d807428a1b7ef79626fdc1c463b46e0879887
Czapa <gesio888lol@wp.pl>
2019-08-10 23:52:06 +0200

    Make possible hiding gui interface from gui.xml

--- src/Gui/xmlGuiParser.hpp
+++ src/Gui/xmlGuiParser.hpp
@@ -10,32 +10,34 @@
 
 namespace ph {
 
-	class GameData;
-	class Xml;
-	class GUI;
-	class Widget;
-	class TextWidget;
+class GameData;
+class Xml;
+class GUI;
+class Widget;
+class TextWidget;
 
-	class XmlGuiParser
-	{
-	public:
-		static void setActionsParser(std::unique_ptr<GuiActionsParser> actionsParser);
+class XmlGuiParser
+{
+public:
+	static void setActionsParser(std::unique_ptr<GuiActionsParser> actionsParser);
 
-		void parseFile(GameData* const gameData, const std::string& fileName);
-		void parseInterface(const Xml& interfaceTag, GUI& gui);
-		void parseWidgetAttributes(const Xml& widgetTag, Widget& widget);
-		void parseTextWidgetAttributes(const Xml& textWidgetTag, TextWidget& widget);
-		void parseWidgetChildren(const Xml& widgetTag, Widget& widget);
+	void parseFile(GameData* const gameData, const std::string& fileName);
+	void parseInterface(const Xml& interfaceTag, GUI& gui);
+	void handleInterfaceHideAttribute(const std::string& interfaceName, const Xml& interfaceTag, GUI& gui);
+	void parseWidgetAttributes(const Xml& widgetTag, Widget& widget);
+	void parseTextWidgetAttributes(const Xml& textWidgetTag, TextWidget& widget);
+	void parseWidgetChildren(const Xml& widgetTag, Widget& widget);
 		
-		sf::Vector2f getVector(const Xml& widgetTag, const std::string& attributeName);
-		std::pair<std::string, std::string> splitString(const std::string& attributeValue);
+	sf::Vector2f getVector(const Xml& widgetTag, const std::string& attributeName);
+	std::pair<std::string, std::string> splitString(const std::string& attributeValue);
 		
-		sf::Color getColor(const Xml& widgetTag);
-		sf::Color parseRGB(std::string colorStr);
-		sf::Color parseRGBA(std::string colorStr);
+	sf::Color getColor(const Xml& widgetTag);
+	sf::Color parseRGB(std::string colorStr);
+	sf::Color parseRGBA(std::string colorStr);
 
-	private:
-		GameData* mGameData;
-		static std::unique_ptr<GuiActionsParser> mActionsParser;
-	};
+private:
+	GameData* mGameData;
+	static std::unique_ptr<GuiActionsParser> mActionsParser;
+};
+
 }
