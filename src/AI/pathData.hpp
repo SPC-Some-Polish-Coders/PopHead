1ea1957c96fb6df3236f8be1ed384746c551cb06
Piotr Gardocki <pgardocki@interia.pl>
2019-08-17 19:16:36 +0200

    Add new directions

--- src/AI/pathData.hpp
+++ src/AI/pathData.hpp
@@ -6,8 +6,18 @@
 namespace ph{
 
 using ObstacleGrids = std::vector<std::vector<bool>>;
+enum class Direction
+{
+	north,
+	north_east,
+	east,
+	south_east,
+	south,
+	south_west,
+	west,
+	north_west
+};
 
-enum class Direction { east, west, north, south, none };
 using Path = std::deque<Direction>;
 
 }
