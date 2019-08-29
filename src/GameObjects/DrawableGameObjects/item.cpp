e39f1d759c16f8e9beee92ebadef16f8150cd472
Szymon Strugala <szymonstrugala23@gmail.com>
2019-08-29 13:08:09 +0200

    Bump Item class

--- src/GameObjects/DrawableGameObjects/item.cpp
+++ src/GameObjects/DrawableGameObjects/item.cpp
@@ -6,12 +6,24 @@ namespace ph {
 
 Item::Item(GameData* const gameData, const std::string& name)
 	:DrawableGameObject(gameData->getRenderer(), name, LayerID::floorEntities)
+	,mClickable(false)
+	,mInInventory(false)
 {
 }
 
 void Item::onUse()
 {
 	PH_LOG_INFO("This item has no usage");
+}
+
+void Item::onPickUp()
+{
+	PH_LOG_INFO("This item has no pick up reaction");
+}
+
+void Item::onDrop()
+{
+	PH_LOG_INFO("This item has no drop reaction");
 }
 
 }
--- src/GameObjects/DrawableGameObjects/item.hpp
+++ src/GameObjects/DrawableGameObjects/item.hpp
@@ -12,7 +12,16 @@ public:
 	Item(GameData* const gameData, const std::string& name);
 
 	virtual void onUse();
+	virtual void onPickUp();
+	virtual void onDrop();
 	//Some other virtuals
+
+	void setClickable(const bool clickable) { mClickable = clickable; }
+	bool getClickable() { return mClickable; }
+
+private:
+	bool mClickable;
+	bool mInInventory;
 };
 
 }
