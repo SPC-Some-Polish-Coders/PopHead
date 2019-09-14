904172a2beb421b45fa0f9ceeb68a7ed63a4711f
Czapa <gesio888lol@wp.pl>
2019-09-14 17:21:25 +0200

    Bump melee attack
    Change melee weapon damage, range and rotation range

--- src/GameObjects/DrawableGameObjects/Characters/player.cpp
+++ src/GameObjects/DrawableGameObjects/Characters/player.cpp
@@ -70,7 +70,11 @@ Player::Player(GameData* gameData)
 {
 	mAnimation.animate(mSprite);
 	addChild(std::make_unique<Gun>(mGameData->getSoundPlayer(), mGameData->getTextures().get("textures/others/pistol.png"), 5.f));
-	addChild(std::make_unique<MeleeWeapon>(mGameData, 25.f, 25.f, 60.f));
+
+	const float meleeWeaponDamage = 35.f;
+	const float range = 27.f;
+	const float rotationRange = 100.f;
+	addChild(std::make_unique<MeleeWeapon>(mGameData, meleeWeaponDamage, range, rotationRange));
 
 	removeChild("Equipement");
 	addChild(std::make_unique<PlayerEquipement>());
