898b18d2f55b70dd6de84f0965995ac75cefaf88
Czapa <gesio888lol@wp.pl>
2019-10-08 15:24:35 +0200

    Make perfect pixel transforms

--- resources/shaders/basic.vs.glsl
+++ resources/shaders/basic.vs.glsl
@@ -10,6 +10,9 @@ uniform mat4 viewProjectionMatrix;
 
 void main()
 {
-	gl_Position = viewProjectionMatrix * modelMatrix * vec4(aPos.x, aPos.y, 0.0, 1.0);
+	mat4 perfectPixelModelMatrix = modelMatrix;
+	perfectPixelModelMatrix[3][0] = float(int(modelMatrix[3][0]));
+	perfectPixelModelMatrix[3][1] = float(int(modelMatrix[3][1]));
+	gl_Position = viewProjectionMatrix * perfectPixelModelMatrix * vec4(aPos.x, aPos.y, 0.0, 1.0);
 	texCoords = mTextureCoords;
 };
