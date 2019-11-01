4b199ee68e2472f10de5609e7c823500d793835a
Piotr Gardocki <pgardocki@interia.pl>
2019-11-01 15:21:06 +0100

    Add temporarily ECS draft

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,21 @@
 #include "Gui/xmlGuiParser.hpp"
 #include "Gui/guiActionsParserImpl.hpp"
 
+#include "Events/actionEventManager.hpp"
+
+//#include "ECS/System.hpp"
+#include "ECS/Systems/movement.hpp"
+#include "ECS/Systems/rendererSystem.hpp"
+#include "ECS/Systems/spritesSync.hpp"
+#include "ECS/Systems/playerInput.hpp"
+
+#include "ECS/systemsQueue.hpp"
+#include "ECS/Components/physicsComponents.hpp"
+#include "ECS/Components/graphicsComponents.hpp"
+#include "ECS/Components/charactersComponents.hpp"
+
+#include <iostream>
+
 int main()
 {
 	try {
@@ -39,6 +54,48 @@ int main()
 		ph::showErrorMessageBox("Error", "Unknown error occurred!");
 		throw;
 	}
+
+	/*ph::ActionEventManager::init();
+
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Test");
+	sf::Texture texture;
+	texture.loadFromFile("resources/textures/vehicles/car.png");
+
+	using namespace ph;
+
+	entt::registry reg;
+
+	ph::SystemsQueue queue(reg);
+	queue.appendSystem<system::PlayerInput>();
+	queue.appendSystem<system::Movement>();
+	queue.appendSystem<system::SpritesSync>();
+	queue.appendSystem<system::Renderer>(std::ref(window));
+
+	component::Sprite spr;
+	spr.sprite.setTexture(texture);
+
+	auto entity = reg.create();
+	reg.assign<component::Position>(entity, 0.f, 0.f);
+	reg.assign<component::Velocity>(entity, 0.f, 0.f);
+	reg.assign<component::Sprite>(entity, std::move(spr));
+	reg.assign<component::CharacterSpeed>(entity, 55.f);
+	reg.assign<component::Player>(entity);
+	
+	sf::Clock clock;
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		sf::Time delta = clock.restart();
+		queue.update(delta.asSeconds());
+	}
+
+	std::cin.get();*/
 
 	return 0;
 }
