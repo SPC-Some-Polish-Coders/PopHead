f852294882aba9036395d4d6101a81d7aac7e55a
Piotr Gardocki <pgardocki@interia.pl>
2019-11-21 23:13:24 +0100

    Fix compiler warning

--- src/Renderer/glEnums.cpp
+++ src/Renderer/glEnums.cpp
@@ -31,6 +31,8 @@ unsigned toGLEnum(DataUsage allocationMode)
 		case ph::DataUsage::streamDraw: return GL_STREAM_DRAW;
 		case ph::DataUsage::staticDraw: return GL_STATIC_DRAW;
 		case ph::DataUsage::dynamicDraw: return GL_DYNAMIC_DRAW;
+		default:
+			PH_UNEXPECTED_SITUATION("This allocation mode is not supported!");
 	}
 }
 
