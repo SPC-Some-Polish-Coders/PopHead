61ac5c29d3d08e0e8fb244849a7e9c77da3b4825
Czapa <gesio888lol@wp.pl>
2019-11-24 02:31:19 +0100

    Cut useless comments and includes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,13 @@
 #include "game.hpp"
-
 #include "Logs/logs.hpp"
-#include "Gui/messageBox.hpp"
-
-#include <stdexcept>
-#include <string>
-
 #include "Logs/logger.hpp"
 #include "Logs/logsInitializing.hpp"
-
 #include "Gui/xmlGuiParser.hpp"
 #include "Gui/guiActionsParserImpl.hpp"
-
-#include "Events/actionEventManager.hpp"
-
 #include "Utilities/profiling.hpp"
-
-//#include "ECS/System.hpp"
-#include "ECS/Systems/movement.hpp"
-#include "ECS/Systems/playerInput.hpp"
-
-#include "ECS/systemsQueue.hpp"
-#include "ECS/Components/physicsComponents.hpp"
-#include "ECS/Components/charactersComponents.hpp"
-
-#include <iostream>
-
-#include <vector>
+#include "Gui/messageBox.hpp"
+#include <stdexcept>
+#include <string>
 
 int main()
 {
@@ -63,48 +44,6 @@ int main()
 		ph::showErrorMessageBox("Error", "Unknown error occurred!");
 		throw;
 	}
-
-	/*ph::ActionEventManager::init();
-
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Test");
-	sf::Texture texture;
-	texture.loadFromFile("resources/textures/vehicles/car.png");
-
-	using namespace ph;
-
-	entt::registry reg;
-
-	ph::SystemsQueue queue(reg);
-	queue.appendSystem<system::PlayerInput>();
-	queue.appendSystem<system::Movement>();
-	queue.appendSystem<system::SpritesSync>();
-	queue.appendSystem<system::Renderer>(std::ref(window));
-
-	component::Sprite spr;
-	spr.sprite.setTexture(texture);
-
-	auto entity = reg.create();
-	reg.assign<component::Position>(entity, 0.f, 0.f);
-	reg.assign<component::Velocity>(entity, 0.f, 0.f);
-	reg.assign<component::Sprite>(entity, std::move(spr));
-	reg.assign<component::CharacterSpeed>(entity, 55.f);
-	reg.assign<component::Player>(entity);
-	
-	sf::Clock clock;
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		sf::Time delta = clock.restart();
-		queue.update(delta.asSeconds());
-	}
-
-	std::cin.get();*/
 
 	return 0;
 }
