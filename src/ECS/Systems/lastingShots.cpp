6dc464907f051a56699f5dbdeb99b805c19ccebe
Czapa <gesio888lol@wp.pl>
2019-12-01 00:58:17 +0100

    Bump gun shot colors

--- src/ECS/Systems/lastingShots.cpp
+++ src/ECS/Systems/lastingShots.cpp
@@ -13,8 +13,8 @@ void LastingShots::update(float seconds)
 	const auto lastingShotsView = mRegistry.view<component::LastingShot>();
 
 	lastingShotsView.each([](const component::LastingShot& lastingShotDetails) {
-		Renderer::submitLine(sf::Color(255, 51, 0), sf::Color::White, lastingShotDetails.startingShotPos, lastingShotDetails.endingShotPos, 1.f);
-		});
+		Renderer::submitLine(sf::Color(230, 160, 0), sf::Color(250, 250, 200), lastingShotDetails.startingShotPos, lastingShotDetails.endingShotPos, 1.f);
+	});
 }
 
 }
