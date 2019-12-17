eccf3bf92533b28c726645dac5f3cb915d1c1f5c
Czapa <gesio888lol@wp.pl>
2019-12-17 15:40:45 +0100

    Refactor and Optimize Game class

--- src/game.hpp
+++ src/game.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
+#include "gameData.hpp"
 #include "Audio/Music/musicPlayer.hpp"
 #include "Audio/Sound/soundPlayer.hpp"
-#include "gameData.hpp"
 #include "AI/aiManager.hpp"
 #include "Scenes/sceneManager.hpp"
 #include "Resources/resourceHolder.hpp"
-#include "Physics/physicsEngine.hpp"
 #include "Terminal/terminal.hpp"
 #include "DebugCounter/debugCounter.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -29,19 +28,17 @@ private:
 	void update(sf::Time dt);
 
 private:
-	std::unique_ptr< GameData >           mGameData;
-	sf::RenderWindow                      mWindow;
-	std::unique_ptr< SoundPlayer >        mSoundPlayer;
-	std::unique_ptr< MusicPlayer >        mMusicPlayer;
-	std::unique_ptr< TextureHolder >      mTextures;
-	std::unique_ptr< FontHolder >         mFonts;
-	std::unique_ptr< AIManager >        mAIManager;
-	std::unique_ptr< SceneManager >       mSceneManager;
-	//std::unique_ptr< Map >              mMap;
-	//std::unique_ptr< PhysicsEngine >    mPhysicsEngine;
-	std::unique_ptr< Terminal >           mTerminal;
-	std::unique_ptr< DebugCounter >       mDebugCounter;
-	std::unique_ptr< GUI >                mGui;
+	sf::RenderWindow               mWindow;
+	std::unique_ptr<GameData>      mGameData;
+	std::unique_ptr<SoundPlayer>   mSoundPlayer;
+	std::unique_ptr<MusicPlayer>   mMusicPlayer;
+	std::unique_ptr<TextureHolder> mTextures;
+	std::unique_ptr<FontHolder>    mFonts;
+	std::unique_ptr<AIManager>     mAIManager;
+	std::unique_ptr<SceneManager>  mSceneManager;
+	std::unique_ptr<Terminal>      mTerminal;
+	std::unique_ptr<DebugCounter>  mDebugCounter;
+	std::unique_ptr<GUI>           mGui;
 };
 
 }
