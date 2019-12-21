def457c88c5e2d5e9c53e38a97a1e3d662d0abc0
Czapa <gesio888lol@wp.pl>
2019-12-21 16:16:46 +0100

    Bump Random

--- src/Utilities/random.cpp
+++ src/Utilities/random.cpp
@@ -1,28 +1,32 @@
 #include "random.hpp"
 #include "Logs/logs.hpp"
+#include <random>
+#include <ctime>
 
-namespace ph {
+namespace ph::Random {
 
-float Random::generateNumber(const float min, const float max)
+static std::default_random_engine engine = std::default_random_engine(static_cast<unsigned>(time(nullptr)));
+
+float generateNumber(const float min, const float max)
 {
 	PH_ASSERT(min <= max, "Min can't be greater than max");
 	std::uniform_real_distribution<float> dist(min, max);
-	return dist(mEngine);
+	return dist(engine);
 }
 
-int Random::generateNumber(const int min, const int max)
+int generateNumber(const int min, const int max)
 {
 	PH_ASSERT(min <= max, "Min can't be greater than max");
 	std::uniform_int_distribution<> dist(min, max);
-	return dist(mEngine);
+	return dist(engine);
 }
 
-sf::Vector2f Random::generateVector(const sf::Vector2f min, const sf::Vector2f max)
+sf::Vector2f generateVector(const sf::Vector2f min, const sf::Vector2f max)
 {
 	PH_ASSERT(min.x <= max.x && min.y <= max.y, "Min can't be greater than max");
 	std::uniform_real_distribution<float> distX(min.x, max.x);
 	std::uniform_real_distribution<float> distY(min.y, max.y);
-	return sf::Vector2f(distX(mEngine), distY(mEngine));
+	return sf::Vector2f(distX(engine), distY(engine));
 }
 
 }
--- src/Utilities/random.hpp
+++ src/Utilities/random.hpp
@@ -1,19 +1,10 @@
 #pragma once
 
-#include <random>
-#include <ctime>
 #include <SFML/System/Vector2.hpp>
 
-namespace ph {
+namespace ph::Random {
 
-class Random {
-public:
-	static float generateNumber(const float min, const float max);
-	static int generateNumber(const int min, const int max);
-	static sf::Vector2f generateVector(const sf::Vector2f min, const sf::Vector2f max);
-
-private:
-	inline static std::default_random_engine mEngine = std::default_random_engine(static_cast<unsigned>(time(nullptr)));
-};
-
+	float generateNumber(const float min, const float max);
+	int generateNumber(const int min, const int max);
+	sf::Vector2f generateVector(const sf::Vector2f min, const sf::Vector2f max);
 }
