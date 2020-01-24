e97c092866b00163acdb8569c7f0d83966e93c37
Czapa <gesio888lol@wp.pl>
2020-01-24 18:01:00 +0100

    Uncomment code in HintAreas

--- src/ECS/Systems/hintAreas.cpp
+++ src/ECS/Systems/hintAreas.cpp
@@ -37,29 +37,24 @@ void HintAreas::update(float dt)
 				auto* hintBackground = mGui.getInterface("hints")->getWidget("hintBackground");
 				auto* hintContent = static_cast<TextWidget*>(hintBackground->getWidget("hintContent"));
 				hintContent->setText(hint.content);
-				
 
-				//mGui.getInterface("hints")->getWidget("canvas")->getWidget(hintDetails.hintName.c_str())->show();
-
-				//// NOTE: This is temporary
-				//if(hintDetails.hintName == "controlHint") {
-				//	ActionEventManager::setActionEnabled("changeWeapon", false);
-				//	ActionEventManager::setActionEnabled("gunAttack", false);
-				//	ActionEventManager::setActionEnabled("meleeAttack", false);
-				//}
-				//else if(hintDetails.hintName == "shootingHint") {
-				//	ActionEventManager::setActionEnabled("gunAttack", true);
-				//}
-				//else if(hintDetails.hintName == "meleeFightingHint") {
-				//	ActionEventManager::setActionEnabled("meleeAttack", true);
-				//}
-				//else if(hintDetails.hintName == "weaponChangingHint") {
-				//	ActionEventManager::setActionEnabled("changeWeapon", true);
-				//}
+				if(hint.hintName == "controlHint") {
+					ActionEventManager::setActionEnabled("changeWeapon", false);
+					ActionEventManager::setActionEnabled("gunAttack", false);
+					ActionEventManager::setActionEnabled("meleeAttack", false);
+				}
+				else if(hint.hintName == "shootingHint") {
+					ActionEventManager::setActionEnabled("gunAttack", true);
+				}
+				else if(hint.hintName == "meleeFightingHint") {
+					ActionEventManager::setActionEnabled("meleeAttack", true);
+				}
+				else if(hint.hintName == "weaponChangingHint") {
+					ActionEventManager::setActionEnabled("changeWeapon", true);
+				}
 			}
 			else if(hint.isShown)
 			{
-				//mGui.getInterface("hints")->getWidget("canvas")->getWidget(hintDetails.hintName.c_str())->hide();
 				mGui.hideInterface("hints");
 				hint.isShown = false;
 			}
