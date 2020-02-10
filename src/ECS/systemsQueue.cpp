85bdf16fe6fd953cf18ee108d5836ab99085acdc
Piotr Gardocki <pgardocki@interia.pl>
2020-02-10 23:39:12 +0100

    Bump SystemsQueue
    Systems are allowed to run in parallel.

--- src/ECS/systemsQueue.cpp
+++ src/ECS/systemsQueue.cpp
@@ -11,8 +11,37 @@ namespace ph {
 
 	void SystemsQueue::update(float seconds)
 	{
-		for (auto& item : mSystemsArray)
-			item.system->update(seconds);
+		size_t currentOrder = 0;
+		auto itemIter = mSystemsArray.begin();
+		std::vector<std::unique_ptr<system::System>*> currentSystems;
+		std::vector<std::future<void>> tasks;
+
+		while (true)
+		{
+			if (itemIter == mSystemsArray.end() || itemIter->order != currentOrder)
+			{
+				for (size_t i = 1; i < currentSystems.size(); ++i)
+				{
+					tasks.emplace_back(mThreadPool.addTask([seconds, &currentSystems, i] { currentSystems[i]->get()->update(seconds); }));
+				}
+
+				if (!currentSystems.empty())
+					currentSystems[0]->get()->update(seconds);
+
+				for (auto& task : tasks)
+					task.get();
+
+				currentSystems.clear();
+				tasks.clear();
+				++currentOrder;
+			}
+
+			if (itemIter == mSystemsArray.end())
+				break;
+
+			currentSystems.push_back(&itemIter->system);
+			++itemIter;
+		}
 	}
 
 	void SystemsQueue::handleEvents(Event e)
