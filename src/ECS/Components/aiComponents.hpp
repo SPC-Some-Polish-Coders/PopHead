9092321949906867d7b0b4e2762316a23c5aa2ca
Piotr Gardocki <pgardocki@interia.pl>
2020-04-21 22:27:17 +0200

    Add SlowZombieBehaviour component

--- src/ECS/Components/aiComponents.hpp
+++ src/ECS/Components/aiComponents.hpp
@@ -13,5 +13,17 @@ namespace ph::component {
 		float timeFromLastGrowl;
 		float timeToMoveToAnotherTile;
 	};
-}
 
+	struct SlowZombieBehavior
+	{
+		constexpr static float farFromPlayerSpeed = 50.f;
+		constexpr static float sneakingSpeed = 50.f;
+		constexpr static float attackingSpeed = 50.f;
+		constexpr static float afterAttackSpeed = 50.f;
+
+		constexpr static float farDistance = 60.f;
+		constexpr static float closeDistance = 30.f;
+
+		float timeFromLastAttack = 0.f;
+	};
+}
